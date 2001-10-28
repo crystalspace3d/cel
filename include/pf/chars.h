ce700f16f141e1b18fa47ab7707a09efed5fd47d
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2001-10-28 08:53:10 +0000

    Started work on the 'pvinv' factory for inventories. This inventory supports two property classes: 'pcinventory' and 'pccharacteristics'. The 'pccharacteristics' is typically used for entities that are put in the inventory. The entity that has the inventory property class will use the characteristics to see if the object fits in the inventory.

---
+++ include/pf/chars.h
@@ -0,0 +1,62 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2001 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CEL_PF_CHARS__
+#define __CEL_PF_CHARS__
+
+#include "cstypes.h"
+#include "csutil/scf.h"
+
+struct iCelEntity;
+
+SCF_VERSION (iPcCharacteristics, 0, 0, 1);
+
+/**
+ * This is a characteristics property class.
+ */
+struct iPcCharacteristics : public iBase
+{
+  /**
+   * Set characteristic property.
+   */
+  virtual void SetCharProperty (const char* name, float value) = 0;
+
+  /**
+   * Get characteristic property.
+   */
+  virtual float GetCharProperty (const char* name) const = 0;
+
+  /**
+   * Clear a property.
+   */
+  virtual void ClearProperty (const char* name) = 0;
+
+  /**
+   * Returns true if a property is present.
+   */
+  virtual bool HasProperty (const char* name) const = 0;
+
+  /**
+   * Clear all properties.
+   */
+  virtual void ClearAll () = 0;
+};
+
+#endif // __CEL_PF_CHARS__
+
---
+++ include/pf/inv.h
@@ -0,0 +1,108 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2001 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CEL_PF_INV__
+#define __CEL_PF_INV__
+
+#include "cstypes.h"
+#include "csutil/scf.h"
+
+struct iCelEntity;
+
+SCF_VERSION (iPcInventory, 0, 0, 1);
+
+/**
+ * This is an inventory property class.
+ */
+struct iPcInventory : public iBase
+{
+  /**
+   * Add an entity. Returns false if the entity could
+   * not be added (capacity exceeded for example).
+   * Note that it is safe to add entities that are already in the inventory.
+   * Nothing will happen then and true will be returned.
+   */
+  virtual bool AddEntity (iCelEntity* entity) = 0;
+
+  /**
+   * Remove an entity.
+   */
+  virtual void RemoveEntity (iCelEntity* entity) = 0;
+
+  /**
+   * Remove all entities.
+   */
+  virtual void RemoveAll () = 0;
+
+  /**
+   * Get the number of entities in this inventory.
+   */
+  virtual int GetEntityCount () const = 0;
+
+  /**
+   * Get some entity.
+   */
+  virtual iCelEntity* GetEntity (int idx) const = 0;
+
+  /**
+   * If true reject any entities that don't have this
+   * characteristic. By default this is false
+   * which means that the inventory will automatically assume
+   * valid values when this characteristic is not present.
+   */
+  virtual void SetStrictCharacteristics (const char* charName, bool strict) = 0;
+
+  /**
+   * Return the value of 'strict characteristics'.
+   */
+  virtual bool HasStrictCharacteristics (const char* charName) const = 0;
+
+  /**
+   * Set the constraints for some characteristic. There is
+   * a min and max value for one entity and also a total max
+   * value for the entire inventory. WARNING! Changing
+   * constraints when the inventory contains items in such a
+   * way that the current contents violates the new constraints
+   * will have no effect on the contents!
+   */
+  virtual void SetConstraints (const char* charName, float minValue, float maxValue,
+		  float totalMaxValue) = 0;
+
+  /**
+   * Get the constraints for some characteristic.
+   * Returns false if there are no constraints defined.
+   */
+  virtual bool GetConstraints (const char* charName, float& minValue, float& maxValue,
+		  float& totalMaxValue) const = 0;
+
+  /**
+   * Get the current value for some characteristic.
+   */
+  virtual float GetCurrentCharacteristic (const char* charName) const = 0;
+  
+  /**
+   * Test if adding a given entity would work with the current constraints.
+   * If not the name of the violating characteristic is returned. Otherwise
+   * NULL is returned.
+   */
+  virtual const char* TestAddEntity (iCelEntity* entity) = 0;
+};
+
+#endif // __CEL_PF_INV__
+
