50b53ec727c5fe50b9a6b981ec470df2fd7dc637
matzebraun <matzebraun@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2001-12-11 18:50:14 +0000

    added comments to the hack from this morning

--- cel/trunk/include/pl/facttmpl.h
+++ cel/trunk/include/pl/facttmpl.h
@@ -1,5 +1,31 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2001 by Jorrit Tyberghein
+    This file was written by Matze Braun <MatzeBraun@gmx.de>
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
+#ifndef __FACTTMPL_H__
+#define __FACTTMPL_H__
 
+/**
+ * This macro declares a factory class for a property class. Note that your
+ * Property Class name must be of the form celPcMyName if you've given
+ * MyName as Factory Name.
+ */
 #define CEL_DECLARE_FACTORY(name)					    \
 class celPf##name : public iCelPropertyClassFactory			    \
 {									    \
@@ -21,6 +47,14 @@ public:									    \
   } scfiComponent;							    \
 };
 
+/** 
+ * This macro implements a property class factory, the first argument is the
+ * name you've given at the declaration, the 2nd argument is the string the
+ * factory will be registered with.
+ * Note: You have to export the classtable yourself as one could have multiple
+ * factories in 1 file. The name to export for the factory MyFactory is:
+ *   celPfMyFactory
+ */
 #define CEL_IMPLEMENT_FACTORY(name, strname)				    \
 SCF_IMPLEMENT_FACTORY (celPf##name)					    \
 SCF_IMPLEMENT_IBASE (celPf##name)					    \
@@ -60,5 +94,5 @@ bool celPf##name::Component::Initialize (iObjectRegistry* p)		    \
 					    \
 }  
 
-
+#endif
 
