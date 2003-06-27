9fd0016892a14167bb1a7bc69e5c67dcdf3c7e5e
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-06-27 07:27:14 +0000

    - Jorrit added apps/celpython. The purpose of this application will 	  be to have a very thin app that only initializes CEL and fires 	  up a python script then. The python script will take over from there 	  and set up the game entities and everything related to the game.

---
+++ cel/trunk/apps/celpython/celpython.h
@@ -0,0 +1,68 @@
+/*
+    Copyright (C) 2003 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __CELPYTHON_H__
+#define __CELPYTHON_H__
+
+#include <stdarg.h>
+
+struct iEngine;
+struct iLoader;
+struct iGraphics3D;
+struct iKeyboardDriver;
+struct iVirtualClock;
+struct iObjectRegistry;
+struct iEvent;
+struct iSector;
+struct iView;
+class csVector3;
+
+struct iPcCamera;
+struct iCelEntity;
+struct iCelPlLayer;
+struct iCelBlLayer;
+struct iCelPropertyClass;
+struct iCelPropertyClassFactory;
+
+class CelPython
+{
+public:
+  iObjectRegistry* object_reg;
+
+private:
+  csRef<iCelPlLayer> pl;
+  csRef<iCelBlLayer> blpython;
+  csRef<iEngine> engine;
+  csRef<iKeyboardDriver> kbd;
+  csRef<iGraphics3D> g3d;
+ 
+  static bool CelPythonEventHandler (iEvent& ev);
+  bool HandleEvent (iEvent& ev);
+  void SetupFrame ();
+  void FinishFrame ();
+
+public:
+  CelPython ();
+  ~CelPython ();
+
+  bool Initialize (int argc, const char* const argv[]);
+  void Start ();
+};
+
+#endif // __CELPYTHON_H__
+
