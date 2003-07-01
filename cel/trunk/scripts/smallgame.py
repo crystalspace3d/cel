ca728442fb510e950f44ad6f43e999943948c263
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-07-01 13:42:26 +0000

    - Jorrit removed the code from celCreateCamera (in python behaviour 	  layer) to set the region. This should be done in the python script 	  and not there. Also removed the 'world' parameter since this is 	  no longer needed. 	- The smallgame.py script now loads partsys and displays it.

--- cel/trunk/scripts/smallgame.py
+++ cel/trunk/scripts/smallgame.py
@@ -4,7 +4,19 @@ class smallgame:
 	def __init__(self,celEntity):
 		print "Initializing game..."
 		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.region")
+		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.mesh")
+		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.solid")
+		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.camera")
 		region = celCreateRegion(physicallayer_ptr,celEntity,"pcregion");
 		print region
+		region.SetWorldFile("/lev/partsys", "world")
+		region.SetRegionName("partsys")
+		rc = region.Load()
+		print rc
+
+		camera = celCreateCamera(physicallayer_ptr,celEntity,"pccamera")
+		print camera
+		camera.SetRegion(region)
+
 
 
