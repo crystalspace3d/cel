544543401fdfda49448f0f535270da3f2996bd4f
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-07-03 09:28:40 +0000

    - Jorrit cleaned up the python plugin a bit. 	- Box in smallgame now has gravity.

--- cel/trunk/scripts/box.py
+++ cel/trunk/scripts/box.py
@@ -5,16 +5,16 @@ class box:
 		print "Initializing box..."
 	def real_init(self,celEntity,args):
 		from smallgame import room
-		#solid = celCreateSolid(physicallayer_ptr,celEntity)
-		#gravity = celCreateGravity(physicallayer_ptr,celEntity)
-		#movable = celCreateMovable(physicallayer_ptr,celEntity)
+		solid = celCreateSolid(physicallayer_ptr,celEntity)
+		gravity = celCreateGravity(physicallayer_ptr,celEntity)
+		movable = celCreateMovable(physicallayer_ptr,celEntity)
 		mesh = celCreateMesh(physicallayer_ptr,celEntity)
 		# @@@ Ugly hardcoding of path!
 		mesh.LoadMesh("fclosed", "/this/apps/celtest/data/box")
 		pos = csVector3 (2, 2, 5)
 		mesh.MoveMesh(room,pos)
 
-		#gravity.SetWeight (1)
-		#v = csVector3 (0, -9.8, 0)
-		#gravity.ApplyPermanentForce (v)
+		gravity.SetWeight (1)
+		v = csVector3 (0, -9.8, 0)
+		gravity.ApplyPermanentForce (v)
 
