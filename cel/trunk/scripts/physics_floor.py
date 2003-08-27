816a0ce5ab02e5aaa7efc5766d63d90ef4870cab
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-08-27 06:23:06 +0000

    - Jorrit moved the apps/celtest/data directory to data so that the 	  python scripts don't have to fetch stuff inside celtest directory.

--- cel/trunk/scripts/physics_floor.py
+++ cel/trunk/scripts/physics_floor.py
@@ -6,7 +6,7 @@ class physics_floor:
 	def real_init(self,celEntity,room,dynsys):
 		#mesh = celCreateMesh(physicallayer_ptr,celEntity)
 		# @@@ Ugly hardcoding of path!
-		#mesh.LoadMesh("box", "/this/apps/celtest/data/box")
+		#mesh.LoadMesh("box", "/this/data/box")
 		#pos = csVector3 (1,-1,5)
 		#mesh.MoveMesh(room,pos)
 
