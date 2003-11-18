e891773ac80bbf725729ca17a67dc3879891b189
vengeance2001 <vengeance2001@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-11-18 00:56:56 +0000

    Separated pc linmove from collision detection code in preparation for making a new linmove pc for sprcal3d sprites.  This way they can both share the same CD code.  Linmove will auto-create iPcCollisionDetection if one is not supplied to it in InitCD().  If you want one other than the default, supply one here.

--- plugins/propclass/move/linmove.cpp
+++ plugins/propclass/move/linmove.cpp
@@ -57,6 +57,7 @@
 #include "physicallayer/persist.h"
 #include "behaviourlayer/behave.h"
 #include "propclass/camera.h"
+#include "propclass/colldet.h"
 #include "propclass/solid.h"
 
 #include "plugins/propclass/move/linmove.h"
@@ -141,13 +142,6 @@ celPcLinearMovement::celPcLinearMovement (iObjectRegistry* object_reg)
     return;
   }
 
-  cdsys = CS_QUERY_REGISTRY (object_reg, iCollideSystem);
-  if (!cdsys)
-  {
-    Report (object_reg, "iCollideSystem missing!");
-    return;
-  }
-
   pl = CS_QUERY_REGISTRY (object_reg, iCelPlLayer);
   if (!pl)
   {
@@ -164,14 +158,13 @@ celPcLinearMovement::celPcLinearMovement (iObjectRegistry* object_reg)
   bottomCollider = 0;
 
   pcmesh = 0;
+  pccolldet = 0;
   path = 0;
   path_speed = 0;
   path_time  = 0;
 
   DG_TYPE (this, "celPcLinearMovement ()");
 
-  useCD = false;
-  onground = false;
   ready = false;
   /*
    * Speed affects all aspects of movement, including gravity.
@@ -235,7 +228,7 @@ bool celPcLinearMovement::Load (iCelDataBuffer* databuf)
   shift.y = cd->value.v.y;
   shift.z = cd->value.v.z;
 
-  if (!InitCD (topSize, bottomSize, shift))
+  if (!InitCD (topSize, bottomSize, shift, NULL))
     return false;
 
   cd = databuf->GetData (3);
@@ -326,13 +319,6 @@ bool celPcLinearMovement::RotateV (float delta)
   return true;
 }
 
-/*
- * Terminal velocity
- * ((120 miles/hour  / 3600 second/hour) * 5280 feet/mile)
- *     / 3.28 feet/meter = 53.65 m/s
- *   *2.0 for "feel" correction = 107.3m/s
- */
-#define ABS_MAX_FREEFALL_VELOCITY 107.3f
 
 /*
  * MAX_CD_INTERVAL is now the maximum amount of time that should pass in a
@@ -373,7 +359,7 @@ bool celPcLinearMovement::MoveSprite (float delta)
   // Err on the side of safety
   local_max_interval -= 0.005f;
 
-  if (useCD)
+  if (pccolldet)
   {
     while (delta > local_max_interval)
     {
@@ -396,7 +382,7 @@ bool celPcLinearMovement::MoveSprite (float delta)
     }
   }
 
-  if (!useCD || delta)
+  if (!pccolldet || delta)
   {
     if (MoveV (delta))
       rc = true;
@@ -408,7 +394,7 @@ bool celPcLinearMovement::MoveSprite (float delta)
 // Do the actual move
 bool celPcLinearMovement::MoveV (float delta)
 {
-  if (vel < SMALL_EPSILON && onground)
+  if (vel < SMALL_EPSILON && (!pccolldet || pccolldet->IsOnGround()))
     return false;  // didn't move anywhere
 
   delta *= speed;
@@ -423,148 +409,16 @@ bool celPcLinearMovement::MoveV (float delta)
 
   newpos = mat*(vel*delta) + oldpos;
 
-  //---------- COLLISION DETECTION STARTS HERE-------------------
-  if (useCD)
+  // Check for collisions and adjust position
+  if (pccolldet)
   {
-    int hits, i;
-    csMatrix3 identitymatrix;
-    csOrthoTransform transform_newpos (identitymatrix, newpos);
-    iSector* current_sector=movable->GetSectors ()->Get (0);
-
-    // We need to measure slightly above the position of the actor or else
-    // we won't really cross a portal.
-    csVector3 temppos = oldpos;
-    //temppos.y+=0.1f;
-    csOrthoTransform transform_oldpos = csReversibleTransform (
-    	csMatrix3(), temppos);
-
-    // Part1: find body collisions => movement
-    // Find possible colliding sectors.
-    csVector3 localvel = mat*(vel * delta);
-
-    num_our_cd = hits = 0;
-
-    // Travel all relevant sectors and do collision detection.
-    cdsys->SetOneHitOnly (false);
-    cdsys->ResetCollisionPairs ();
-    hits += CollisionDetect (topCollider, current_sector,
-    	&transform_newpos, &transform_oldpos);
-
-    for (i = 0; i < num_our_cd; i++ )
-    {
-      csCollisionPair& cd = our_cd_contact[i];
-      csVector3 vec = ((cd.c2-cd.b2)%(cd.b2-cd.a2)).Unit ();
-      if (vec * localvel < 0 )
-    	continue;
-
-      localvel = -(localvel % vec) % vec;
-    }
-    newpos = oldpos + localvel;
-
-    // Part2: legs
-    num_our_cd = hits = 0;
-    transform_newpos = csOrthoTransform (csMatrix3(), newpos);
-
-    cdsys->SetOneHitOnly (false);
-    cdsys->ResetCollisionPairs ();	
-    hits += CollisionDetect (bottomCollider, current_sector,
-    	&transform_newpos, &transform_oldpos);
-
-    if (hits == 0)
-    {
-      // No hits on the bottom bound, we're falling.
-      onground = false;
-      // gravity! move down!
-      vel.y  -= 19.6 * delta;
-      /*
-       * Terminal velocity
-       *   ((120 miles/hour  / 3600 second/hour) * 5280 feet/mile)
-       *   / 3.28 feet/meter = 53.65 m/s
-       */
-      if (vel.y < -(ABS_MAX_FREEFALL_VELOCITY))
-    	vel.y = -(ABS_MAX_FREEFALL_VELOCITY);
-
-    }
-    else
-    {
-      float max_y = -1e10;
-
-      for (i = 0; i < num_our_cd; i++ )
-      {
-    	csCollisionPair cd = our_cd_contact[i];
-    	csVector3 n = ((cd.c2-cd.b2)%(cd.b2-cd.a2)).Unit ();
-
-    	// Is it a collision with a ground polygon?
-    	//  (this tests for the angle between ground and collidet
-    	//  triangle)
-    	if (-n.y < 0.7)
-    	  continue;
-
-    	csVector3 line[2];
-    	cd.a1 += newpos;
-    	cd.b1 += newpos;
-    	cd.c1 += newpos;
-    	if (FindIntersection (cd,line))
-    	{
-    	  if (line[0].y > max_y)
-	    max_y = line[0].y;
-    	  if (line[1].y > max_y)
-	    max_y = line[1].y;
-    	}
-      }
-
-      float p = newpos.y - max_y + 0.01;
-      if (ABS (p) < bottomSize.y - 0.01)
-      {
-    	if (max_y != -1e10)
-    	  newpos.y = max_y - 0.01;
-
-    	if (vel.y < 0 )
-    	  vel.y = 0;
-      }
-
-      onground = true;
-    }
-
-    // Part 2.5: check top again and revert move if we're still in a wall.
-    num_our_cd = hits = 0;
-    cdsys->SetOneHitOnly (false);
-    cdsys->ResetCollisionPairs ();
-    transform_newpos = csOrthoTransform (csMatrix3(), newpos);
-
-    if (CollisionDetect (topCollider, current_sector,
-    	&transform_newpos,&transform_oldpos) > 0)
-    {
-      newpos = oldpos;
-    }
-    else
-    {
-      // Part 3: portal traversal.
-      iSector* new_sector = movable->GetSectors ()->Get (0);
-      iSector* old_sector = new_sector;
-
-      temppos = newpos;
-      // @@@ Jorrit: had to do this add!
-      float height5 = (bottomSize.y + topSize.y) / 20.0;
-      temppos.y += height5;
-      transform_oldpos.SetOrigin (transform_oldpos.GetOrigin ()
-      	+ csVector3 (0, height5, 0));
-      bool mirror = false;
-
-      new_sector = new_sector->FollowSegment (transform_oldpos,
-      	temppos, mirror, CEL_LINMOVE_FOLLOW_ONLY_PORTALS);
-      if (new_sector != old_sector)
-    	movable->SetSector (new_sector);
-    }
-
+    pccolldet->AdjustForCollisions(oldpos,newpos,vel,delta,movable);
   }
-
-  //---------- COLLISION END HERE-------------------
 
   // move to the new position
   movable->GetTransform ().SetOrigin (newpos);
 
-  if (useCD)
+  if (pccolldet)
   {
     // Part 4: Add us to all nearby sectors.
     pcmesh->GetMesh ()->PlaceMesh ();
@@ -653,13 +507,13 @@ bool celPcLinearMovement::HandleEvent (iEvent& ev)
   return false;
 }
 
-bool celPcLinearMovement::InitCD ()
+bool celPcLinearMovement::InitCD (iPcCollisionDetection *pc_cd)
 {
-  return InitCD (topSize, bottomSize, shift);
+  return InitCD (topSize, bottomSize, shift, pc_cd);
 }
 
 bool celPcLinearMovement::InitCD (const csVector3& body, const csVector3& legs,
-	const csVector3& shift)
+	const csVector3& shift,iPcCollisionDetection *pc_cd)
 {
   if (!pcmesh)
   {
@@ -681,208 +535,36 @@ bool celPcLinearMovement::InitCD (const csVector3& body, const csVector3& legs,
   bottomSize = legs;
   celPcLinearMovement::shift = shift;
 
-  csRef<iPolygonMesh> mesh;
-
-  float bX2 = body.x / 2.0f;
-  float bZ2 = body.z / 2.0f;
-  float bYbottom = legs.y;
-  float bYtop = legs.y + body.y;
-
-  mesh = csPtr<iPolygonMesh> (new csPolygonMeshBox (
-	csBox3 (csVector3 (-bX2, bYbottom, -bZ2) + shift,
-		csVector3 (bX2, bYtop, bZ2) + shift)));
-  topCollider = cdsys->CreateCollider (mesh);
-
-  float lX2 = legs.x / 2.0f;
-  float lZ2 = legs.z / 2.0f;
-
-  mesh = csPtr<iPolygonMesh> (new csPolygonMeshBox (
-	csBox3 (csVector3 (-lX2, LEGOFFSET, -lZ2) + shift,
-		csVector3 (lX2, LEGOFFSET + legs.y, lZ2) + shift)));
-  bottomCollider = cdsys->CreateCollider (mesh);
-
-  bool result;
-  if (topCollider && bottomCollider)
-    result = true;
-  else
-    result = false;
-
-  if (!result)
+  if (!pc_cd)
   {
-    Report (object_reg, "Error while setting up CD!");
-    return result;
-  }
+    csRef<iCelPropertyClass> pc;
+    pc = pl->CreatePropertyClass(entity, "pccollisiondetection");
+    if ( !pc )
+    {
+        Report (object_reg, "Could not create property class pccollisiondetection.");
+        return false;
+    }
+    csRef<iPcCollisionDetection> pctemp;
+    pctemp = SCF_QUERY_INTERFACE(pc, iPcCollisionDetection);
 
-  useCD = true;
-  return result;
-}
-
-iCollider* celPcLinearMovement::FindCollider (iObject* object)
-{
-  csColliderWrapper* wrap = csColliderWrapper::GetColliderWrapper (object);
-  if (wrap)
-  {
-    return wrap->GetCollider ();
+    pccolldet = pctemp;
   }
   else
   {
-    // There is no collider wrapper. In this case we test if there
-    // is a pcsolid for the entity. If not we'll add a null collider.
-    iCelEntity* ent = pl->FindAttachedEntity (object);
-    if (ent)
-    {
-      csRef<iPcSolid> pcsolid = CEL_QUERY_PROPCLASS (
-            ent->GetPropertyClassList (), iPcSolid);
-      if (pcsolid)
-      {
-	// Calling pcsolid->GetCollider() will cause a csColliderWrapper
-	// to be created.
-        return pcsolid->GetCollider ();
-      }
-    }
-    // Add a null collider so we don't check for pcsolid again.
-    csRef<csColliderWrapper> cw = csPtr<csColliderWrapper> (
-	  	new csColliderWrapper (object, cdsys, (iCollider*)0));
+    pccolldet = pc_cd;
   }
-  return 0;
+
+  return pccolldet->Init(topSize,bottomSize,shift);
 }
 
-/*
- * Iterates over the nearby entities to find collisions
- */
-int celPcLinearMovement::CollisionDetect (iCollider *collider,
-    				  iSector* sector,
-    				  csReversibleTransform* transform,
-    				  csReversibleTransform* old_transform)
+bool celPcLinearMovement::IsOnGround ()
 {
-  int hits = 0;
-  int j;
-  // This never changes during this function, calculate it early
-  csVector3 newpos=transform->GetO2TTranslation ();
+  if (pccolldet)
+    return pccolldet->IsOnGround();
 
-  csCollisionPair* CD_contact;
-
-  csRef<iMeshWrapperIterator> objectIter = engine->GetNearbyMeshes (sector,
-    	transform->GetOrigin (),
-    	COLLISION_DISTANCE,
-    	true);
-
-  while (objectIter->HasNext ())
-  {
-    iMeshWrapper* meshWrapper = objectIter->Next ();
-    // Avoid hitting the mesh from this entity itself.
-    if (meshWrapper != pcmesh->GetMesh ())
-    {
-      cdsys->ResetCollisionPairs ();
-      csReversibleTransform tr = meshWrapper->GetMovable ()
-      	->GetFullTransform ();
-      iCollider* othercollider = FindCollider (meshWrapper->QueryObject ());
-      if (othercollider && cdsys->Collide (collider,
-      	transform, othercollider, &tr))
-      {
-    	bool reallycollided = false;
-
-#ifdef SHOW_COLLIDER_MESH_DEBUG
-	//@@@
-    	Notify2("Player in Sector  : %s",
-    	  sector->QueryObject ()->GetName ());
-    	Notify2("Collider Mesh Name  : %s",
-    	  meshWrapper->QueryObject ()->GetName ());
-    	Notify2("Collider Mesh Sector: %s",
-    	  meshWrapper->GetMovable ()->GetSectors ()->Get (0)->QueryObject ()
-	  	->GetName ());
-#endif
-
-    	CD_contact = cdsys->GetCollisionPairs ();
-    	for (j = 0; j < cdsys->GetCollisionPairCount (); j++ )
-    	{
-    	  /*
-	   * Here we follow a segment from our current position to the
-	   * position of the collision. If the sector the collision occured
-	   * in is not the sector of the mesh we collided with,
-    	   * this is invalid.
-    	   */
-    	  int sector_idx, sector_max;
-    	  iSector* CollisionSector;
-    	  bool mirror=false;
-    	  csVector3 testpos;
-    	  csVector3 line[2];
-    	  csCollisionPair temppair=CD_contact[j];
-
-    	  // Move the triangle from our mesh into world space
-    	  temppair.a1 += newpos;
-    	  temppair.b1 += newpos;
-    	  temppair.c1 += newpos;
-
-    	  if (FindIntersection (temppair, line))
-    	  {
-	    // Collided at this line segment. Pick a point in the middle of
-	    // the segment to test.
-	    testpos=(line[0]+line[1])/2;
-    	  }
-	  else
-	  {
-	    // No collision found, use the destination of the move.
-	    testpos=newpos;
-    	  }
-
-    	  // This follows a line segment from start to finish and returns
-	  // the sector you are ultimately in.
-    	  CollisionSector = sector->FollowSegment (*old_transform,
-	  	testpos, mirror, CEL_LINMOVE_FOLLOW_ONLY_PORTALS);
-
-    	  // Iterate through all the sectors of the destination mesh,
-	  // incase it's in multiple sectors.
-    	  sector_max = meshWrapper->GetMovable ()->GetSectors ()->GetCount ();
-    	  for (sector_idx=0 ; sector_idx<sector_max ; sector_idx++)
-    	  {
-	    // Check to see if this sector is the sector of the collision.
-	    if (CollisionSector == meshWrapper->GetMovable ()->GetSectors ()
-	    	->Get (sector_idx))
-	    {
-	      reallycollided = true;
-	      our_cd_contact[num_our_cd++] = CD_contact[j];
-	      // One valid sector is enough
-	      break;
-	    }
-#ifdef LINMOVE_CD_FOLLOWSEG_DEBUG
-	    else
-	    {
-	      printf ("Failed FollowSegment test on mesh %s in sector %s."
-	      	" FollowSegment ended in sector %s\n",
-		meshWrapper->QueryObject ()->GetName (),
-		meshWrapper->GetMovable ()->GetSectors ()->Get (sector_idx)
-			->QueryObject ()->GetName (),
-		CollisionSector->QueryObject ()->GetName ());
-	      csVector3 cv_oldpos, cv_newpos;
-	      cv_oldpos = old_transform->GetO2TTranslation ();
-	      cv_newpos = transform->GetO2TTranslation ();
-	      printf ("Followed from sector %s position: %f,%f,%f"
-	      	"  to sector %s position: %f,%f,%f\n",
-		sector->QueryObject ()->GetName (),
-		cv_oldpos.x, cv_oldpos.y, cv_oldpos.z,
-		CollisionSector->QueryObject ()->GetName (),
-		cv_newpos.x, cv_newpos.y, cv_newpos.z);
-	    }
-#endif
-    	  }
-    	}
-
-    	// We don't increase hits unless a collision really occurred after
-	// all tests.
-    	if (reallycollided)
-	{
-    	  hits++;
-	}
-
-    	if (cdsys->GetOneHitOnly () && hits)
-    	  return 1;
-      }
-    }
-  }
-
-  return hits;
+  return false;
 }
+
 
 /**
  * WARNING:  At present time this function does not work correctly!
@@ -942,7 +624,10 @@ bool celPcLinearMovement::SetDRData (iDataBuffer* data,bool detectcheat, csStrin
   if ((*ptr) & LINMOVE_PATH_FLAG)
     return SetPathDRData (data);
   else
-    onground = *ptr++;
+    if (pccolldet)
+      pccolldet->SetOnGround(*ptr++);
+    else
+      ptr++;
 
   float* tptr = (float*) ptr;
 
@@ -1228,7 +913,7 @@ csPtr<iDataBuffer> celPcLinearMovement::GetDRData (csStringHash* msgstrings)
     pcmesh->GetMesh ()->GetMovable ()->GetTransform ().GetT2O ();
 
   char *ptr = (char *) databuf->GetData ();
-  *ptr++ = (onground)?1:0;
+  *ptr++ = (pccolldet)?pccolldet->IsOnGround():0;
 
   float* tptr = (float*)ptr;
 
