93cb9fe67cf077f4b47de351e37d7bb0d9a21674
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2003-12-01 18:50:24 +0000

    Compile fixes.

--- plugins/behaviourlayer/xml/token.cpp
+++ plugins/behaviourlayer/xml/token.cpp
@@ -17,6 +17,7 @@
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+#include <ctype.h>
 #include "cssysdef.h"
 #include "csutil/objreg.h"
 #include "plugins/behaviourlayer/xml/token.h"
