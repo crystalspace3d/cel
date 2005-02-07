be4b3beb5a0b8dcf3ac567db61074efbc9a93ff6
res2002 <res2002@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-02-07 01:33:21 +0000

    Automated Swig Python file repair.

--- cel/trunk/scripts/blcelc.py
+++ cel/trunk/scripts/blcelc.py
@@ -412,6 +412,8 @@ class csString(_object):
     def GetDataSafe(*args): return _blcelc.csString_GetDataSafe(*args)
     def Length(*args): return _blcelc.csString_Length(*args)
     def IsEmpty(*args): return _blcelc.csString_IsEmpty(*args)
+    def FindStr(*args): return _blcelc.csString_FindStr(*args)
+    def FindReplace(*args): return _blcelc.csString_FindReplace(*args)
     def StartsWith(*args): return _blcelc.csString_StartsWith(*args)
     def __init__(self, *args):
         _swig_setattr(self, csString, 'this', _blcelc.new_csString(*args))
@@ -4117,6 +4119,8 @@ class iEngine(iBase):
     def GetCurrentDefaultRenderloop(*args): return _blcelc.iEngine_GetCurrentDefaultRenderloop(*args)
     def SetCurrentDefaultRenderloop(*args): return _blcelc.iEngine_SetCurrentDefaultRenderloop(*args)
     def GetCurrentFrameNumber(*args): return _blcelc.iEngine_GetCurrentFrameNumber(*args)
+    def SetSaveableFlag(*args): return _blcelc.iEngine_SetSaveableFlag(*args)
+    def GetSaveableFlag(*args): return _blcelc.iEngine_GetSaveableFlag(*args)
     def __del__(self, destroy=_blcelc.delete_iEngine):
         try:
             if self.thisown: destroy(self)
@@ -9205,7 +9209,6 @@ class iTextureHandle(iBase):
     CS_TEXTURE_CUBE_NEG_Z = _blcelc.iTextureHandle_CS_TEXTURE_CUBE_NEG_Z
     def GetRendererDimensions(*args): return _blcelc.iTextureHandle_GetRendererDimensions(*args)
     def GetOriginalDimensions(*args): return _blcelc.iTextureHandle_GetOriginalDimensions(*args)
-    def SetTextureTarget(*args): return _blcelc.iTextureHandle_SetTextureTarget(*args)
     def GetTextureTarget(*args): return _blcelc.iTextureHandle_GetTextureTarget(*args)
     def Blit(*args): return _blcelc.iTextureHandle_Blit(*args)
     def GetImageName(*args): return _blcelc.iTextureHandle_GetImageName(*args)
