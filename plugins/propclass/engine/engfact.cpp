f642c875051a3524237b9ef142fff8589421058f
sunshine <sunshine@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-05-02 19:06:02 +0000

    Eliminated compilation warnings.

--- plugins/propclass/engine/engfact.cpp
+++ plugins/propclass/engine/engfact.cpp
@@ -313,7 +313,7 @@ bool celPcRegion::Load (bool allow_entity_addon)
 
   // If we don't allow the entity addon to work then we mark this here
   // in the physical layer.
-  bool prev_allow_entity_addon;
+  bool prev_allow_entity_addon = false;
   if (!allow_entity_addon)
   {
     prev_allow_entity_addon = pl->IsEntityAddonAllowed ();
