7aa595d578f7ec57b93f8fd9f102fbcc98a1994e
sunshine <sunshine@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-06-10 21:30:23 +0000

    Eric Sunshine fixed computation of the script's own directory in start_smallgame.sh, which was almost completely borked. It failed in all but the simplest cases.

--- cel/trunk/start_smallgame.sh
+++ cel/trunk/start_smallgame.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-myPath=`echo $0 | sed -e "s/\/.*//"`
-./bootstrap cel.behaviourlayer.python ${myPath}/scripts/smallgame/smallgame $*
+
+# Eric's poor-man (portable) 'dirname' command.
+dirpart() {
+  dirpart_d=`expr "$1" : '\(..*\)[/\\].*'`
+  if test -n "${dirpart_d}"; then
+    echo ${dirpart_d}
+  else
+    echo '.'
+  fi
+}
+
+mydir=`dirpart $0`
+./bootstrap cel.behaviourlayer.python ${mydir}/scripts/smallgame/smallgame $*
