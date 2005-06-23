1704413ce612b909c5e404fe07ffe8e6f1660424
darekdawidowski <darekdawidowski@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-06-23 22:25:06 +0000

    - Dariusz Dawidowski added new XML actions for pczonemanager.

--- cel/trunk/include/propclass/zone.h
+++ cel/trunk/include/propclass/zone.h
@@ -1,17 +1,17 @@
 /*
     Crystal Space Entity Layer
     Copyright (C) 2004 by Jorrit Tyberghein
-  
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-  
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-  
+
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -215,6 +215,10 @@ SCF_VERSION (iPcZoneManager, 0, 1, 1);
  * to get the ID of the parameter):
  * <ul>
  * <li>Load: parameters 'path' (string), and 'file' (string).
+ * <li>PointMesh: parameters 'entity' (string), 'region' (string),
+ *    and 'start' (string).
+ * <li>PointCamera: parameters 'entity' (string), 'region' (string),
+ *    and 'start' (string).
  * </ul>
  * <p>
  * This property class can send out the following messages
