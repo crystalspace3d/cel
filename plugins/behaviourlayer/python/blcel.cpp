96c9b090a98e0abef566f8bef65c35cfe4ff6c49
crystal-manage <crystal-manage@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-10-18 01:15:12 +0000

    Automated Swig Python file repair.

--- plugins/behaviourlayer/python/blcel.cpp
+++ plugins/behaviourlayer/python/blcel.cpp
@@ -18754,6 +18754,27 @@ static PyObject *_wrap_scfQuery_iPcCamera(PyObject *, PyObject *args) {
 }
 
 
+static PyObject *_wrap_iPcDefaultCamera_SetMesh(PyObject *, PyObject *args) {
+    PyObject *resultobj;
+    iPcDefaultCamera *arg1 = (iPcDefaultCamera *) 0 ;
+    iPcMesh *arg2 = (iPcMesh *) 0 ;
+    PyObject * obj0 = 0 ;
+    PyObject * obj1 = 0 ;
+    
+    if(!PyArg_ParseTuple(args,(char *)"OO:iPcDefaultCamera_SetMesh",&obj0,&obj1)) goto fail;
+    SWIG_Python_ConvertPtr(obj0, (void **)&arg1, SWIGTYPE_p_iPcDefaultCamera, SWIG_POINTER_EXCEPTION | 0);
+    if (SWIG_arg_fail(1)) SWIG_fail;
+    SWIG_Python_ConvertPtr(obj1, (void **)&arg2, SWIGTYPE_p_iPcMesh, SWIG_POINTER_EXCEPTION | 0);
+    if (SWIG_arg_fail(2)) SWIG_fail;
+    (arg1)->SetMesh(arg2);
+    
+    Py_INCREF(Py_None); resultobj = Py_None;
+    return resultobj;
+    fail:
+    return NULL;
+}
+
+
 static PyObject *_wrap_iPcDefaultCamera_SetMode__SWIG_0(PyObject *, PyObject *args) {
     PyObject *resultobj;
     iPcDefaultCamera *arg1 = (iPcDefaultCamera *) 0 ;
@@ -25453,6 +25474,7 @@ static PyMethodDef SwigMethods[] = {
 	 { (char *)"celCreateCamera", _wrap_celCreateCamera, METH_VARARGS, NULL},
 	 { (char *)"celGetCamera", _wrap_celGetCamera, METH_VARARGS, NULL},
 	 { (char *)"scfQuery_iPcCamera", _wrap_scfQuery_iPcCamera, METH_VARARGS, NULL},
+	 { (char *)"iPcDefaultCamera_SetMesh", _wrap_iPcDefaultCamera_SetMesh, METH_VARARGS, NULL},
 	 { (char *)"iPcDefaultCamera_SetMode", _wrap_iPcDefaultCamera_SetMode, METH_VARARGS, NULL},
 	 { (char *)"iPcDefaultCamera_GetMode", _wrap_iPcDefaultCamera_GetMode, METH_VARARGS, NULL},
 	 { (char *)"iPcDefaultCamera_SetModeName", _wrap_iPcDefaultCamera_SetModeName, METH_VARARGS, NULL},
