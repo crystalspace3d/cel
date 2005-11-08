c9119fab7998435fca9dfd5864f2e27f48b1eab2
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2005-11-08 10:02:05 +0000

    - Jorrit added LGPL notice to all mechanics headers. 	- Jorrit fixed a small memory leak in pcmechobject. 	- Jorrit added a new iPcMechanicsJoint property class. 	- Jorrit removed the now obsolete CreateJoint action in 	  the mechanics system.

--- cel/trunk/include/propclass/mechsys.h
+++ cel/trunk/include/propclass/mechsys.h
@@ -1,3 +1,22 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2005 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #ifndef __CEL_PF_MECHANICS_SYSTEM__
 #define __CEL_PF_MECHANICS_SYSTEM__
 
@@ -32,12 +51,6 @@ struct iJoint;
  * <li>QuickStep: no parameters.
  * <li>EnableStepFast: no parameters.
  * <li>DisableStepFast: no parameters.
- * <li>CreateJoint: parameters 'body1' (string), 'body2' (string),
- *     'position' (vector3), 'constraindistx' (bool), 'constraindisty' (bool),
- *     'constraindistz' (bool), 'mindistance' (vector3),
- *     'maxdistance' (vector3), 'constrainanglex' (bool),
- *     'constrainangley' (bool), 'constrainanglez' (bool),
- *     'minangle' (vector3), and 'maxangle' (vector3).
  * </ul>
  */
 struct iPcMechanicsSystem : public virtual iBase
@@ -575,6 +588,31 @@ struct iPcMechanicsObject : public virtual iBase
    * Create a joint between this object and another.
    */
   virtual iJoint* CreateJoint (iPcMechanicsObject* other) = 0;
+};
+
+/**
+ * A joint.
+ * <p>
+ * This property class supports the following actions (add prefix
+ * 'cel.action.' to get the ID of the action and add prefix 'cel.parameter.'
+ * to get the ID of the parameter):
+ * <ul>
+ * <li>SetParentBody: parameters 'body' (string).
+ * <li>SetPosition: parameters 'position' (vector3).
+ * <li>SetConstrainDist: parameters 'x' (bool), 'y' (bool), and 'z' (bool).
+ * <li>SetDistances: parameters 'min' (vector3) and 'max' (vector3).
+ * <li>SetConstrainAngle: parameters 'x' (bool), 'y' (bool), and 'z' (bool).
+ * <li>SetAngles: parameters 'min' (vector3) and 'max' (vector3).
+ * </ul>
+ */
+struct iPcMechanicsJoint : public virtual iBase
+{
+  SCF_INTERFACE (iPcMechanicsJoint, 0, 0, 1);
+
+  /**
+   * Get the joint corresponding with this.
+   */
+  virtual iJoint* GetJoint () = 0;
 };
 
 #endif // __CEL_PF_MECHANICS_SYSTEM__
