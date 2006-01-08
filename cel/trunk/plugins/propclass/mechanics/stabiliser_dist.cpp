bfd8a6278a421fa0cd23cc9bf9fe9fd86dbccdf8
genjix <genjix@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-01-08 11:56:41 +0000

    made the functions reference counted and made them adhere to cs coding style more.

--- cel/trunk/plugins/propclass/mechanics/stabiliser_dist.cpp
+++ cel/trunk/plugins/propclass/mechanics/stabiliser_dist.cpp
@@ -12,111 +12,100 @@
     #define M_E 2.71828182845904523536
 #endif
 
-IntervalMetaDistribution::~IntervalMetaDistribution()
+void IntervalMetaDistribution::Add(csRef<StabiliserFunction> func , float low_int , float high_int)
 {
-	for(std::vector<Interval>::iterator i = funcs.begin() ; i != funcs.end() ; i++)
-		delete i->func;
-}
+  Interval i;
+  i.low = low_int;
+  i.high = high_int;
+  i.func = func;
 
-void IntervalMetaDistribution::Add(StabiliserFunction *func , float low_int , float high_int)
-{
-	Interval i;
-	i.low = low_int;
-	i.high = high_int;
-	i.func = func;
-
-	funcs.push_back(i);
+  funcs.Push(i);
 }
 
 float IntervalMetaDistribution::Force(float h)
 {
-	for(std::vector<Interval>::iterator i = funcs.begin() ; i != funcs.end() ; i++)
-		if( ((i->low < h) && (i->high > h)) || (i->low == h) || (i->high == h) )
-			return i->func->Force(h);
-	return 0;
+  for(csArray<Interval>::Iterator it = funcs.GetIterator() ; it.HasNext() ; ) {
+    Interval i = it.Next();
+
+    if( ((i.low < h) && (i.high > h)) || (i.low == h) || (i.high == h) )
+      return i.func->Force(h);
+  }
+
+  return 0;
 }
 
 
 IfFallingDistribution::IfFallingDistribution(csRef<iPcMechanicsObject> ship,
-	StabiliserFunction *ifdist , StabiliserFunction *elsedist , float adelta)
+  csRef<StabiliserFunction> ifdist , csRef<StabiliserFunction> elsedist , float adelta)
 {
-	ship_mech = ship;
-	if_dist = ifdist;
-	else_dist = elsedist;
-	delta = adelta;
-}
-IfFallingDistribution::~IfFallingDistribution()
-{
-	delete if_dist;
-	delete else_dist;
+  ship_mech = ship;
+  if_dist = ifdist;
+  else_dist = elsedist;
+  delta = adelta;
 }
 
 float IfFallingDistribution::Force(float h)
 {
-	//printf("fall: %f\t",ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y);
-	if(ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y < delta)
-		return if_dist->Force(h);
-	return else_dist->Force(h);
+  //printf("fall: %f\t",ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y);
+  if(ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y < delta)
+    return if_dist->Force(h);
+  return else_dist->Force(h);
 }
 
-FallingMultiplierDistribution::FallingMultiplierDistribution(csRef<iPcMechanicsObject> ship , StabiliserFunction *adist)
+FallingMultiplierDistribution::FallingMultiplierDistribution(csRef<iPcMechanicsObject> ship , csRef<StabiliserFunction> adist)
 {
-	dist = adist;
-	ship_mech = ship;
-}
-FallingMultiplierDistribution::~FallingMultiplierDistribution()
-{
-	delete dist;
+  dist = adist;
+  ship_mech = ship;
 }
 
 float FallingMultiplierDistribution::Force(float h)
 {
-	return ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y * dist->Force(h);
+  return ship_mech->WorldToLocal(ship_mech->GetLinearVelocity()).y * dist->Force(h);
 }
 
 ReturnConstantValue::ReturnConstantValue(float avalue)
 {
-	value = avalue;
+  value = avalue;
 }
 
 float ReturnConstantValue::Force(float h)
 {
-	return value;
+  return value;
 }
 
 
 InversePowerDistribution::InversePowerDistribution(float h0 , float f0 , float h1 , float f1)
 {
-	k = ComputeK(h0, f0, h1, f1);
-	x = ComputeX(h0, f0, k);	// h0 == 1 will fail
+  k = ComputeK(h0, f0, h1, f1);
+  x = ComputeX(h0, f0, k);  // h0 == 1 will fail
 }
 float InversePowerDistribution::Force(float h)
 {
-	return Formula(h);
+  return Formula(h);
 }
 
 float InversePowerDistribution::ComputeK(float h0 , float f0 , float h1 , float f1)
 {
-	return std::pow(float(M_E), (( std::log(h1)*std::log(f0) - std::log(h0)*std::log(f1) ) /
-					( std::log(h1) - std::log(h0) )) );
+  return std::pow(float(M_E), (( std::log(h1)*std::log(f0) - std::log(h0)*std::log(f1) ) /
+          ( std::log(h1) - std::log(h0) )) );
 }
 float InversePowerDistribution::ComputeX(float h , float f , float k)
 {
-	return std::log(k / f) / std::log(h);
+  return std::log(k / f) / std::log(h);
 }
 
 float InversePowerDistribution::Formula(float h)
 {
-	return k / std::pow(h , x);
+  return k / std::pow(h , x);
 }
 
 
 SquareDistribution::SquareDistribution(float h0 , float f0 , float h1 , float f1)
 {
-	k = (f1 - f0) / (h1*h1 - h0*h0);
-	a = f1 - k*h1*h1;
+  k = (f1 - f0) / (h1*h1 - h0*h0);
+  a = f1 - k*h1*h1;
 }
 float SquareDistribution::Force(float h)
 {
-	return a + k*h*h;
+  return a + k*h*h;
 }
