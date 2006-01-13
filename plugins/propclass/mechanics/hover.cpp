86682fb211009910980f394b825ee220b522a039
genjix <genjix@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-01-13 16:59:51 +0000

    improve correlation between fast and slower machines.

--- plugins/propclass/mechanics/hover.cpp
+++ plugins/propclass/mechanics/hover.cpp
@@ -91,8 +91,8 @@ void celPcHover::SetWorld (const char *name)
 void celPcHover::DefaultHeightFunction ()
 {
   csRef<celIntervalMetaDistribution> i(new celIntervalMetaDistribution());
-  csRef<celReturnConstantValue> a(new celReturnConstantValue(6.5));
-  csRef<celSquareDistribution>  b(new celSquareDistribution(1.0001f , 6.0f , 3.0f , 2.0f));
+  csRef<celReturnConstantValue> a(new celReturnConstantValue(65.0f));
+  csRef<celSquareDistribution>  b(new celSquareDistribution(1.0001f , 60.0f , 3.0f , 20.0f));
   csRef<celReturnConstantValue> c(new celReturnConstantValue(0.0));
 
   i->Add(a, -9999999999.0f , 0.0001f);
@@ -100,8 +100,8 @@ void celPcHover::DefaultHeightFunction ()
   i->Add(c, 3.0f , 9999999999.0f);
 
   csRef<celIntervalMetaDistribution> e(new celIntervalMetaDistribution());
-  csRef<celReturnConstantValue> m(new celReturnConstantValue(1.6));
-  csRef<celSquareDistribution>  n(new celSquareDistribution(1.0001f , 1.5f , 3.0f , 0.9f));
+  csRef<celReturnConstantValue> m(new celReturnConstantValue(16.0f));
+  csRef<celSquareDistribution>  n(new celSquareDistribution(1.0001f , 15.0f , 3.0f , 9.0f));
   csRef<celReturnConstantValue> o(new celReturnConstantValue(0.0));
 
   e->Add(m, -9999999999.0f , 0.0001f);
@@ -147,7 +147,8 @@ void celPcHover::PerformStabilising ()
   float force = func->Force (height);
 
   csRef<iPcMechanicsObject> pcmechobj = CEL_QUERY_PROPCLASS_ENT (GetEntity(), iPcMechanicsObject);
-  pcmechobj->AddForceDuration (csVector3 (0,force,0), false, csVector3 (0,0,0), 0.3f);
+  //pcmechobj->AddForceDuration (csVector3 (0,force,0), false, csVector3 (0,0,0), 0.01f);
+  pcmechobj->AddForceOnce (csVector3 (0,force,0), false, csVector3 (0,0,0));
 
   // the ships roll should try to remain level (levels faster when closer to ground)
   if(height < ang_cutoff_height) {
