21038e3fe383e291146af0c9225b01fda65c8884
cfraz89 <cfraz89@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-06-23 14:41:43 +0000

    moving hover into its own plugin

---
+++ cel/trunk/plugins/propclass/hover/craft_controller.cpp
@@ -0,0 +1,171 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2005 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include "cssysdef.h"
+#include "iutil/objreg.h"
+#include "iutil/evdefs.h"
+
+#include "plugins/propclass/hover/craft_controller.h"
+#include "physicallayer/pl.h"
+#include "physicallayer/entity.h"
+#include "physicallayer/persist.h"
+
+#include <propclass/mechsys.h>
+#include <propclass/mesh.h>
+
+#include <iengine/mesh.h>
+#include <ivaria/dynamics.h>
+
+//---------------------------------------------------------------------------
+
+CEL_IMPLEMENT_FACTORY(CraftController, "pccraft")
+
+SCF_IMPLEMENT_IBASE_EXT (celPcCraftController)
+  SCF_IMPLEMENTS_EMBEDDED_INTERFACE (iPcCraftController)
+SCF_IMPLEMENT_IBASE_EXT_END
+
+SCF_IMPLEMENT_EMBEDDED_IBASE (celPcCraftController::PcCraftController)
+  SCF_IMPLEMENTS_INTERFACE (iPcCraftController)
+SCF_IMPLEMENT_EMBEDDED_IBASE_END
+
+celPcCraftController::celPcCraftController (iObjectRegistry* object_reg)
+	: celPcCommon (object_reg), celPeriodicTimer (pl)
+{
+  SCF_CONSTRUCT_EMBEDDED_IBASE (scfiPcCraftController);
+
+  turn_left = false;
+  turn_right = false;
+  turn_up = false;
+  turn_down = false;
+
+  current_turning_left = 0;
+  current_turning_right = 0;
+  current_up = 0;
+  current_down = 0;
+
+  turn_acc = 0.4f;
+  pitch_acc = 0.4f;
+  turn_max = 1.5f;
+  pitch_max = 0.5f;
+
+  thrust_on = false;
+  thrust = 10.0f;
+  topspeed = 20.0f;
+
+  after_burner = false;
+  topburnerspeed = 40.0f;
+
+  slide_on = false;
+  redirect_vel_ratio = 0.0;
+  deceleration_rate = 1.0 - 0.01;
+
+  brakes_on = false;
+  braking_speed = 1.0 - 0.1;
+}
+
+celPcCraftController::~celPcCraftController ()
+{
+}
+
+csPtr<iCelDataBuffer> celPcCraftController::Save ()
+{
+  csRef<iCelDataBuffer> databuf = pl->CreateDataBuffer (1);
+  return csPtr<iCelDataBuffer> (databuf);
+}
+bool celPcCraftController::Load (iCelDataBuffer* databuf)
+{
+  return true;
+}
+bool celPcCraftController::PerformAction (csStringID actionId, iCelParameterBlock* params)
+{
+  return true;
+}
+
+void celPcCraftController::Tick ()
+{
+  UpdateBody ();
+}
+
+
+void celPcCraftController::DoTurningCalc (bool isturning, float &turn, float acc, float max)
+{
+  if(isturning)
+    turn += acc;
+  else
+    //turn -= acc;
+    turn = 0.0f;
+
+  if(turn > max)
+    turn = max;
+  else if(turn < 0.0f)
+    turn = 0.0f;
+}
+
+void celPcCraftController::UpdateBody ()
+{
+  DoTurningCalc (turn_left, current_turning_left, turn_acc, turn_max);
+  DoTurningCalc (turn_right, current_turning_right, turn_acc, turn_max);
+  DoTurningCalc (turn_up, current_up, pitch_acc, pitch_max);
+  DoTurningCalc (turn_down, current_down, pitch_acc, pitch_max);
+
+  /*! at the moment roll in the ships turning is disabled
+      until I write a self angular stabilising component */
+  csVector3 turning(current_up - current_down, current_turning_right
+  	- current_turning_left, 0);
+
+  csRef<iPcMechanicsObject> ship_mech = CEL_QUERY_PROPCLASS_ENT (GetEntity(),
+  	iPcMechanicsObject);
+  float height = 2.0;   // disable height dependence for now
+
+  // angular suppressant is dependant on height
+  // could use multiplier here
+  csVector3 angular_supressant = ship_mech->GetAngularVelocity() * 1;
+
+  if(height > 0.1f)
+    angular_supressant /= height;
+  else
+    angular_supressant /= 0.1f;
+
+  ship_mech->SetAngularVelocity(ship_mech->LocalToWorld(turning)
+  	+ angular_supressant);
+
+  // normalize velocity vector
+  if (!slide_on && redirect_vel_ratio > 0.0)
+  {
+    float speed = ship_mech->GetLinearVelocity ().Norm ();
+    ship_mech->SetLinearVelocity ((1.0 - redirect_vel_ratio) * ship_mech
+    	->GetLinearVelocity () + redirect_vel_ratio
+	* ship_mech->LocalToWorld (csVector3 (0,0,-speed)));
+  }
+
+  float hspeed = after_burner ? topburnerspeed : topspeed;
+
+  // I hope to move this to another interface
+  csVector3 lv = ship_mech->WorldToLocal (ship_mech->GetLinearVelocity ());
+  if (thrust_on && !(-lv.z > hspeed))
+    ship_mech->AddForceDuration (csVector3 (0,0,-thrust), true,
+      csVector3 (0,0,0), 0.1f);
+  if (!thrust_on)
+    ship_mech->SetLinearVelocity (deceleration_rate *
+      ship_mech->GetLinearVelocity ());
+  // remember: brakes and thruster can be on at same time
+  if (brakes_on)
+    ship_mech->SetLinearVelocity (braking_speed *
+      ship_mech->GetLinearVelocity ());
+}
---
+++ cel/trunk/plugins/propclass/hover/hover.h
@@ -0,0 +1,194 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2005 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __CEL_PF_HOVER_FACTORY__
+#define __CEL_PF_HOVER_FACTORY__
+
+#include "cstypes.h"
+
+#include "physicallayer/propclas.h"
+#include "physicallayer/propfact.h"
+#include "physicallayer/facttmpl.h"
+#include "celtool/stdpcimp.h"
+#include "celtool/stdparams.h"
+
+#include "propclass/hover.h"
+#include "propclass/mesh.h"
+
+#include "tools/stabiliser_dist.h"
+#include "../mechanics/ticktimer.h"
+
+struct iPcMesh;
+struct iPcMechanicsObject;
+struct iVirtualClock;
+
+/**
+ * Factory for hover.
+ */
+CS_IMPLEMENT_PLUGIN
+
+CEL_DECLARE_FACTORY (Hover)
+
+/**
+ * Hover stabiliser property class.
+ */
+class celPcHover : public celPcCommon , public celPeriodicTimer
+{
+public:
+  celPcHover (iObjectRegistry* object_reg);
+  virtual ~celPcHover ();
+
+  SCF_DECLARE_IBASE_EXT (celPcCommon);
+
+  virtual const char* GetName() const { return "pchover"; };
+  virtual csPtr<iCelDataBuffer> Save ();
+  virtual bool Load (iCelDataBuffer* databuf);
+  virtual bool PerformAction (csStringID actionId, iCelParameterBlock* params);
+  virtual void Tick ();
+
+  /**
+   * Should Tick every delta and update the objects
+   * height
+   */
+  virtual void PerformStabilising();
+
+  virtual void SetWorldMesh(csRef<iPcMesh> wmesh) { world_mesh = wmesh; }
+  virtual void SetWorld(const char *name);
+  virtual void SetHeightBeamCutoff(float chm) { height_beam_cutoff = chm; }
+  virtual void SetAngularBeamOffset(float abo) { ang_beam_offset = abo; }
+  virtual void SetAngularCutoffHeight(float ach) { ang_cutoff_height = ach; }
+  virtual void SetAngularCorrectionStrength(float mul) { ang_mult = mul; }
+  virtual void SetStabiliserFunction(celStabiliserFunction *sfunc)
+  { func.AttachNew(sfunc); }
+
+  virtual void UseDefaultFunction (float dampening);
+  virtual float GetHeight () { return object_height; }
+
+  struct PcHover : public iPcHover
+  {
+    SCF_DECLARE_EMBEDDED_IBASE(celPcHover);
+
+    virtual void SetWorldMesh (csRef<iPcMesh> wmesh)
+    {
+      scfParent->SetWorldMesh (wmesh);
+    }
+    virtual void SetWorld (const char *name)
+    {
+      scfParent->SetWorld (name);
+    }
+    virtual void SetHeightBeamCutoff (float chm)
+    {
+      scfParent->SetHeightBeamCutoff (chm);
+    }
+    virtual void SetAngularBeamOffset (float abo)
+    {
+      scfParent->SetAngularBeamOffset (abo);
+    }
+    virtual void SetAngularCutoffHeight (float ach)
+    {
+      scfParent->SetAngularCutoffHeight (ach);
+    }
+    virtual void SetAngularCorrectionStrength (float mul)
+    {
+      scfParent->SetAngularCorrectionStrength (mul);
+    }
+    virtual void SetStabiliserFunction (celStabiliserFunction *sfunc)
+    {
+      scfParent->SetStabiliserFunction (sfunc);
+    }
+
+    virtual void UseDefaultFunction (float dampening)
+    {
+      scfParent->UseDefaultFunction (dampening);
+    }
+
+    virtual float GetHeight ()
+    {
+      return scfParent->GetHeight ();
+    }
+  } scfiPcHover;
+
+private:
+  // Actions
+  static csStringID action_setworld;
+  static csStringID action_sethbeamcutoff;
+  static csStringID action_setangoff;
+  static csStringID action_setangheight;
+  static csStringID action_setangstr;
+  static csStringID action_usedeffunc;
+
+  // Parameters.
+  static csStringID param_world;
+  static csStringID param_hbeamcutoff;
+  static csStringID param_angoff;
+  static csStringID param_angheight;
+  static csStringID param_angstr;
+
+  /**
+   * Calculate height of object to ground (world mesh)
+   */
+  float Height(csVector3 offset = csVector3(0,0,0));
+
+  /**
+   * In case of object not being in range in Height(),
+   * extend another beam upwards to calculate reverse distance,
+   * else return infinity
+   */
+  float ReverseHeight(csVector3 &start);
+
+  /**
+   * This function computes the angle the ship has to roll through to
+   * to be aligned with the terrain
+   *
+   * offset is which offset to use for hit beams to test difference in
+   * heights. i.e csVector3(0,0,-1)
+   *
+   * height is the height at center of ship
+   */
+  float AngularAlignment(csVector3 offset , float height);
+
+  /**
+   * Looks up the world_mesh and gets the csWeakRef.
+   */
+  void LookUpWorldMesh ();
+
+  /// because the ref isn't gotten until first tick
+  csString world_mesh_name;
+  /// mesh used for height calculation
+  csWeakRef<iPcMesh> world_mesh;
+
+  /// offset used for calculating alignment wrt to ground
+  float ang_beam_offset;
+  /// how fast the object aligns its orientation to the ground
+  float ang_mult;
+  /// cutoff height at which not to align with ground
+  float ang_cutoff_height;
+  /// cutoff height at which height test returns inf
+  float height_beam_cutoff;
+  /// function which computes upthrust depending on height
+  csRef<celStabiliserFunction> func;
+
+  /// last calculated height of object
+  float object_height;
+
+  /// stored ship mech interface
+  csWeakRef<iPcMechanicsObject> ship_mech;
+};
+
+#endif
