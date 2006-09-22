114c601d01b16727b4485d9a93538d20df74c285
darekdawidowski <darekdawidowski@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-09-22 23:42:18 +0000

    Doxygen comment.

--- include/propclass/mesh.h
+++ include/propclass/mesh.h
@@ -1,17 +1,17 @@
 /*
     Crystal Space Entity Layer
     Copyright (C) 2001 by Jorrit Tyberghein
-  
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-  
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-  
+
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -64,6 +64,8 @@ class csVector3;
  * 'cel.property.' to get the ID of the property:
  * - position (vector3, read): current position.
  * - fullposition (vector3, read): current full position.
+ * - rotation (vector3, read): current rotation.
+ * - eulerrotation (vector3, read): current euler rotation.
  * - sector (string, read): current sector.
  * - path (string, read): path for model.
  * - factory (string, read): factory for model.
