5bcdf4b618eb5a9a178d6f59230745780a4ead3a
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-11-27 12:38:18 +0000

    - Jorrit merged r2429:2430 into 1.0 (install and python fixes).

--- V1.0/data/library/blpython/actorball.py
+++ V1.0/data/library/blpython/actorball.py
@@ -4,47 +4,32 @@
 
 # it also prints a time out in the tooltip as an example.
 
-from cspace import *
-from blcelc import *
+from pycel import *
 
 class actorball:
 	def __init__(self,celEntity):
-		print "Initializing actor..."
+		print "Initializing actorball...",celEntity.Name
 		# the timeout counter
 		self.counter = 60
 		# movement variables
 		self.forward = 0
 		self.side = 0
 		# make sure we have a tooltip
-		self.tooltip = celGetToolTip(celEntity)
-		if not self.tooltip:
-			self.tooltip = celCreateToolTip(physicallayer_ptr,celEntity)
+		self.tooltip = celToolTip(celEntity)
 		# make sure we have a pcdefaultcamera
-		self.camera = celGetDefaultCamera(celEntity)
-		if not self.camera:
-			self.camera = celCreateDefaultCamera(physicallayer_ptr,celEntity)
+		self.camera = celDefaultCamera(celEntity)
 		# get a virtualclock interface to count time
-		self.myClock = CS_QUERY_REGISTRY (object_reg_ptr, iVirtualClock)
-		self.time = self.myClock.GetCurrentTicks()
+		self.time = Clock.GetCurrentTicks()
 
 		# get a mechanics object
 		self.meshobject = celGetMechanicsObject(celEntity)
-
+		self.mechsystem = self.meshobject.GetMechanicsSystem()
+		self.mechsystem.Gravity = csVector3(0,0,0)
 		# make sure we have a pccommandinput
-		input = celGetCommandInput(celEntity)
-		if not input:
-			input = celCreateCommandInput(physicallayer_ptr,celEntity)
-		# set default camera mode
-		self.camera.SetModeName ("lara_thirdperson")
+		input = celCommandInput(celEntity)
 		# bind inputs
-		input.Bind("up", "forward")
-		input.Bind("down", "backward")
-		input.Bind("a", "lookup")
-		input.Bind("z", "lookdown")
-		input.Bind("m", "cammode")
-		input.Bind(" ", "jump")
-		input.Bind("left", "forteleft")
-		input.Bind("right", "forteright")
+		input.LoadConfig("actorball")
+		self.gravity=False
 	
 	# TRIGGER CALLBACKS
 	def pctrigger_leavetrigger(self,celEntity,args):
@@ -56,18 +41,23 @@ class actorball:
 		pass
 	# TIMER CALLBACKS
 	def pctimer_wakeupframe(self,celEntity,args):
-		g2d=CS_QUERY_REGISTRY(object_reg_ptr,iGraphics2D)
 		if self.counter > 0:
-			self.tooltip.SetText("remaining time:"+str(self.counter))
+			self.tooltip.Text = "remaining time:"+str(self.counter)
 		else:
-			self.tooltip.SetText("GAME OVER")
-		newtime = self.myClock.GetCurrentTicks()
+			self.tooltip.Text = "GAME OVER"
+		newtime = Clock.GetCurrentTicks()
 		if self.forward:
 			self.meshobject.AddForceFrame(csVector3(0,0,self.forward*20),False,csVector3(0,0,0))
 		if self.side:
 			self.meshobject.AddForceFrame(csVector3(self.side*20,0,0), False,csVector3(0,0,0))
 	
 	# LOOK UP/DOWN
+	def pccommandinput_gravity1(self,celEntity,args):
+		if self.gravity:
+			self.mechsystem.Gravity = csVector3(0,0.0,0)
+		else:
+			self.mechsystem.Gravity = csVector3(0,-0.2,0)
+		self.gravity = not self.gravity
 	def pccommandinput_lookup1(self,celEntity,args):
 		self.camera.SetPitchVelocity(1.0)
 
@@ -85,11 +75,11 @@ class actorball:
 
 	#JUMP
 	def pccommandinput_jump1(self,celEntity,args):
-		newtime = self.myClock.GetCurrentTicks()
+		newtime = Clock.GetCurrentTicks()
 		# this try to apply the force only if the object is not
 		# touching the ground
 		if newtime - self.time > -100:
-			self.meshobject.AddForceOnce(csVector3(0,3000,0),False,csVector3(0,0,0))
+			self.meshobject.AddForceOnce(csVector3(0,300,0),False,csVector3(0,0,0))
 		self.time = newtime
 	def pccommandinput_jump0(self,celEntity,args):
 		pass
@@ -123,11 +113,11 @@ class actorball:
 		pass
 	# CAMERA MODES
 	def pccommandinput_cammode1(self,celEntity,args):
-		self.camera.SetMode(self.camera.GetNextMode ())
+		self.camera.Mode = self.camera.GetNextMode ()
 
 	# COLLISION CALLBACK
 	# all pcmechanicobjects have this
 	def pcdynamicbody_collision(self,celEntity,args):
-		newtime = self.myClock.GetCurrentTicks()
+		newtime = Clock.GetCurrentTicks()
 		self.time = newtime
 
--- V1.0/data/library/blpython/npc.py
+++ V1.0/data/library/blpython/npc.py
@@ -5,35 +5,29 @@
 # . 2: will walk reandomly (doesnt stop).
 # also can control simple actions on an spr3d.
 # uses: linmove, actormove, timer, mesh, properties
-from cspace import *
-from blcelc import *
+from pycel import *
 
 import random
 
 class npc:
 	def __init__(self,celEntity):
-		print "Initializing npc..."
-		pl=physicallayer_ptr
+		print "Initializing npc...",celEntity.Name
 		# get or create linmove
-		self.linmove = celGetLinearMovement(celEntity)
-                if not self.linmove:
-                        self.linmove = celCreateLinearMovement(pl,celEntity)
+		self.linmove = celLinearMovement(celEntity)
 		# get or create actormove
-		self.actormove = celGetActorMove(celEntity)
-                if not self.actormove:
-                        self.actormove = celCreateActorMove(pl,celEntity)
-		self.actormove.SetMovementSpeed(1.0)		
-		self.actormove.SetJumpingVelocity (4.1)
+		self.actormove = celActorMove(celEntity)
+		self.actormove.MovementSpeed = 1.0
+		self.actormove.JumpingVelocity = 4.1
 
 		# set timer if there is not one
 		self.timer = celGetTimer(celEntity)
                 if not self.timer:
-                        self.timer = celCreateTimer(pl,celEntity)
+                        self.timer = celAddTimer(celEntity)
                         self.timer.WakeUp (500,True)
 
 		# get pcproperties
-		self.prop = celGetProperties(celEntity)
 		self.thechosen=False
+		self.prop = celGetProperties(celEntity)
 		if self.prop:
 			i=self.prop.GetPropertyIndex("chosen")
 			if not i == None:
@@ -41,7 +35,7 @@ class npc:
 		# check for mesh
                 self.mesh = celGetMesh(celEntity)
                 if self.mesh:
-                	meshobj = self.mesh.GetMesh()
+                	meshobj = self.mesh.Mesh
 			self.spr3dstate = SCF_QUERY_INTERFACE(meshobj.GetMeshObject(), iSprite3DState)
 			if self.spr3dstate:
 				self.spr3dstate.SetAction("walk")
@@ -59,10 +53,10 @@ class npc:
 		pass
 	def pcmeshsel_up(self,celEntity,args):
 		pl = physicallayer_ptr
-		actor = pl.FindEntity("camera")
+		actor = Entities["camera"]
 		toolt = celGetToolTip(actor)
 		if toolt:
-			toolt.SetText(celEntity.GetName())
+			toolt.Text = celEntity.Name
 		
 
 	def pctimer_wakeup(self,celEntity,args):
@@ -75,28 +69,28 @@ class npc:
 		# ROTATE
 		value = random.random()
 		if value<0.3 and is_advancing:
-			self.actormove.RotateLeft(1)
-			self.actormove.RotateRight(0)
+			self.actormove.RotatingLeft = 1
+			self.actormove.RotatingRight = 0
 		elif value<0.8 and is_advancing:
-			self.actormove.RotateLeft(0)
-			self.actormove.RotateRight(1)
+			self.actormove.RotatingLeft = 0
+			self.actormove.RotatingRight = 1
 		else:
-			self.actormove.RotateLeft(0)
-			self.actormove.RotateRight(0)
+			self.actormove.RotatingLeft = 0
+			self.actormove.RotatingRight = 0
 		# the chosen 2 can only run forward
 		if self.thechosen==2:
-			self.actormove.Forward(1)
+			self.actormove.MovingForward = 1
 			return
 		if is_advancing:
-			if not self.actormove.IsMovingForward ():
-				self.actormove.Forward(1)
-				self.actormove.Backward(0)
+			if not self.actormove.MovingForward:
+				self.actormove.MovingForward = 1
+				self.actormove.MovingBackward = 0
 				if self.spr3dstate:
 					self.spr3dstate.SetAction("walk")
 		else:
-			if self.actormove.IsMovingForward ():
-				self.actormove.Forward(0)
-				self.actormove.Backward(0)
+			if self.actormove.MovingForward:
+				self.actormove.MovingForward = 0
+				self.actormove.MovingBackward = 0
 				if self.spr3dstate:
 					self.spr3dstate.SetAction("stand")
 
