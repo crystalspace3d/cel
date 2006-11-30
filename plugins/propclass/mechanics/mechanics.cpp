30d0dab0214c305025be9cef25511cbf6ebd2e2a
darekdawidowski <darekdawidowski@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2006-11-30 16:59:34 +0000

    Fixing SetDynamicSystem.

--- plugins/propclass/mechanics/mechanics.cpp
+++ plugins/propclass/mechanics/mechanics.cpp
@@ -1,17 +1,17 @@
 /*
     Crystal Space Entity Layer
     Copyright (C) 2005 by Jorrit Tyberghein
-  
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-  
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-  
+
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -209,6 +209,12 @@ void celPcMechanicsSystem::SetDynamicSystem (const char* dynsysname)
 {
   GetDynamics();
   dynsystem = dynamics->FindSystem(dynsysname);
+  if (!dynsystem)
+  {
+    dynsystem = dynamics->CreateSystem ();
+    dynsystem->QueryObject ()->SetName (dynsysname);
+    dynsystem->SetGravity (csVector3 (0, -9.8f, 0));
+  }
 }
 
 void celPcMechanicsSystem::SetDynamicSystem (iDynamicSystem* dynsys)
@@ -851,7 +857,7 @@ bool celPcMechanicsObject::Load (iCelDataBuffer* databuf)
         d = databuf->GetFloat ();
 	delete bdata;
 	bdata = new plane_data (csPlane3 (databuf->GetFloat (),
-          databuf->GetFloat (), databuf->GetFloat (), databuf->GetFloat ())); 
+          databuf->GetFloat (), databuf->GetFloat (), databuf->GetFloat ()));
       }
       break;
     case CEL_BODY_MESH:
@@ -1751,7 +1757,7 @@ void celPcMechanicsJoint::CreateJoint ()
     return;	// @@@ Error?
   }
   iRigidBody* body2 = pcmechobj->GetBody ();
-  csRef<iPcMechanicsSystem> mechsystem = 
+  csRef<iPcMechanicsSystem> mechsystem =
   	csQueryRegistry<iPcMechanicsSystem> (object_reg);
   if (!mechsystem)
   {
