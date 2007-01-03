dc1e9c9826a50509655ee1ab6b551628fc7c7f6f
cfraz89 <cfraz89@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-01-03 12:35:46 +0000

    improving pcwheeled

--- cel/trunk/include/propclass/wheeled.h
+++ cel/trunk/include/propclass/wheeled.h
@@ -114,6 +114,7 @@ struct iCelEntity;
 struct iBodyGroup;
 struct iMeshFactoryWrapper;
 struct iODEHinge2Joint;
+struct iODEAMotorJoint;
 struct iRigidBody;
 
 struct iPcWheeled : public virtual iBase
@@ -601,21 +602,27 @@ struct iPcWheeled : public virtual iBase
   virtual bool GetWheelHandbrakeAffected(size_t wheelnum) = 0;
 
   /**
-   * Get the number of wheels on this vehicle
+   * Get the number of wheels on this vehicle.
    */
   virtual size_t GetWheelCount() = 0;
 
   /**
-   * Get whether the rigid body of this wheel
+   * Get the rigid body of this wheel.
    * \param wheelnum Index of the wheel to get.
    */
   virtual iRigidBody* GetWheelBody(size_t wheelnum) = 0;
 
   /**
-   * Get whether the joint connecting this wheel
+   * Get the joint connecting this wheel to the body.
    * \param wheelnum Index of the wheel to get.
    */
   virtual iODEHinge2Joint* GetWheelJoint(size_t wheelnum) = 0;
+
+  /**
+   * Get the motor which brakes this wheel.
+   * \param wheelnum Index of the wheel to get.
+   */
+  virtual iODEAMotorJoint* GetBrakeMotor(size_t wheelnum) = 0;
 
   /**
    * Set whether to send messages back to the entity when wheels collide with
