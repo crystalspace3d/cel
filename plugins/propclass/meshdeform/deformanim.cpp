832173aac896f4e62be8e047dfc7ed32ebf91c69
cfraz89 <cfraz89@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-01-13 12:51:03 +0000

    added meshdeform

---
+++ plugins/propclass/meshdeform/deformanim.cpp
@@ -0,0 +1,122 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2001 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include "cssysdef.h"
+#include "iutil/objreg.h"
+#include "csutil/debug.h"
+#include "plugins/propclass/meshdeform/meshdeformfact.h"
+#include "physicallayer/pl.h"
+#include "physicallayer/entity.h"
+#include "physicallayer/persist.h"
+#include "behaviourlayer/behave.h"
+#include "csgeom/vector3.h"
+#include "csgeom/box.h"
+#include "iengine/mesh.h"
+#include "iengine/engine.h"
+#include "propclass/mesh.h"
+#include "imesh/object.h"
+#include "imesh/objmodel.h"
+#include "imesh/genmesh.h"
+#include "plugins/propclass/meshdeform/deformanim.h"
+#include "csutil/randomgen.h"
+#include <iostream>
+//---------------------------------------------------------------------------
+
+csDeformControlType::csDeformControlType(iBase* parent)
+  : scfImplementationType(this, parent), object_reg(0)
+{
+}
+
+csPtr<iGenMeshAnimationControlFactory>
+  csDeformControlType::CreateAnimationControlFactory()
+{
+  csDeformControlFactory* ctrl = new csDeformControlFactory (this);
+  return csPtr<iGenMeshAnimationControlFactory> (ctrl);
+}
+//---------------------------------------------------------------------------
+
+csDeformControlFactory::csDeformControlFactory(iBase* parent)
+  : scfImplementationType(this, parent), object_reg(0)
+{
+}
+
+csPtr<iGenMeshAnimationControl>
+  csDeformControlFactory::CreateAnimationControl (iMeshObject *mesh)
+{
+  csDeformControl* ctrl = new csDeformControl (this);
+  csRef<iGeneralMeshState> state = scfQueryInterface<iGeneralMeshState> (mesh);
+  state->SetAnimationControl(ctrl);
+  return ctrl;
+}
+
+//---------------------------------------------------------------------------
+
+csDeformControl::csDeformControl(iBase* parent)
+  : scfImplementationType(this, parent)
+{
+  std::cout  << "init";
+  original_verts = 0;
+  deformed_verts = 0;
+  total_verts = 0;
+  mesh = 0;
+  r_gen = csRandomGen();
+}
+
+void csDeformControl::Update(csTicks current)
+{
+}
+
+const csVector3* csDeformControl::UpdateVertices
+  (csTicks current, const csVector3* verts, int num_verts, uint32 version_id)
+{
+  //The mesh has changed, need to (re) initialise
+  if(num_verts != total_verts || !original_verts || !deformed_verts)
+  {
+    std::cout << num_verts << " " << total_verts << " " << original_verts << " " << deformed_verts << "\n";
+    total_verts = num_verts;
+    delete[] original_verts;
+    delete[] deformed_verts;
+    original_verts = new csVector3[total_verts];
+    deformed_verts = new csVector3[total_verts];
+
+    for (int i = 0; i < num_verts; i++)
+    {
+      original_verts[i] = csVector3(verts[i].x, verts[i].y, verts[i].z);
+      deformed_verts[i] = csVector3(verts[i].x, verts[i].y, verts[i].z);
+    }
+  }
+  
+  csVector3* ve = deformed_verts;
+  return ve;
+} 
+
+void csDeformControl::DeformMesh
+(const csVector3& position, const csVector3& direction, float radius)
+{
+  for (int i = 0; i < total_verts; i++)
+  {
+    csVector3 cvert = deformed_verts[i];
+    float distance = (position - cvert).Norm();
+    if (distance < radius)
+    {
+       float r_amount = r_gen.Get();
+       deformed_verts[i] = cvert + (direction * r_amount) ;
+    }
+  }
+}
---
+++ plugins/propclass/meshdeform/deformanim.h
@@ -0,0 +1,110 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2001 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __CEL_PF_DEFORMANIM__
+#define __CEL_PF_DEFORMANIM__
+
+#include "cstypes.h"
+#include "iutil/comp.h"
+#include "csutil/scf.h"
+#include "physicallayer/propclas.h"
+#include "physicallayer/propfact.h"
+#include "physicallayer/facttmpl.h"
+#include "celtool/stdpcimp.h"
+#include "celtool/stdparams.h"
+#include "imesh/genmesh.h"
+#include "csutil/randomgen.h"
+#include "csutil/scf_implementation.h"
+
+struct iMeshWrapper;
+//---- Genmesh Anim support code ------------------------------------
+
+struct iDeformControl : public virtual iBase
+{
+  SCF_INTERFACE (iDeformControl, 0, 0, 1);
+  virtual void SetMesh(iMeshWrapper* mesh) = 0;
+virtual void DeformMesh
+  (const csVector3& position, const csVector3& direction, float amount) = 0;
+};
+
+class csDeformControlType : public scfImplementation1
+  <csDeformControlType, iGenMeshAnimationControlType>
+{
+  public:
+    iObjectRegistry* object_reg;
+    csDeformControlType (iBase* parent);
+    virtual ~csDeformControlType () {}
+    virtual csPtr<iGenMeshAnimationControlFactory>
+    CreateAnimationControlFactory();
+};
+
+class csDeformControlFactory : public scfImplementation1
+  <csDeformControlFactory, iGenMeshAnimationControlFactory>
+{
+  public:
+    iGenMeshAnimationControlType* control;
+    iObjectRegistry* object_reg;
+    csDeformControlFactory (iBase* parent);
+    virtual ~csDeformControlFactory ()
+    {}
+    virtual csPtr<iGenMeshAnimationControl>
+        CreateAnimationControl (iMeshObject *mesh);
+    virtual const char* Load (iDocumentNode* node) {return 0;}
+    virtual const char* Save (iDocumentNode* parent) {return 0;}
+};
+
+class csDeformControl : public scfImplementation2
+  <csDeformControl, iGenMeshAnimationControl, iDeformControl>
+{
+  private:
+    csVector3* original_verts;
+    csVector3* deformed_verts;
+    int total_verts;
+    iMeshWrapper* mesh;
+    csRandomGen r_gen;
+
+  public:
+    csDeformControl (iBase* parent);
+    virtual ~csDeformControl () {}
+    virtual bool AnimatesVertices () const {return true;}
+    virtual bool AnimatesTexels () const {return false;}
+    virtual bool AnimatesNormals () const {return false;}
+    virtual bool AnimatesColors () const {return false;}
+    virtual const csVector3* UpdateVertices (csTicks current,
+    const csVector3* verts, int num_verts, uint32 version_id);
+
+    virtual void Update (csTicks current);
+    virtual const csVector2* UpdateTexels (csTicks current,
+      const csVector2* texels, int num_texels, uint32 version_id)
+    {return texels;}
+    virtual const csVector3* UpdateNormals (csTicks current,
+      const csVector3* normals, int num_normals, uint32 version_id)
+    {return normals;}
+    virtual const csColor4* UpdateColors (csTicks current,
+      const csColor4* colors, int num_colors, uint32 version_id)
+    {return colors;}
+
+    virtual void SetMesh(iMeshWrapper* mesh)
+    {csDeformControl::mesh = mesh;}
+    virtual void DeformMesh
+    (const csVector3& position, const csVector3& direction, float radius);
+
+}; 
+
+#endif // __CEL_PF_DEFORMANIM__
