fe35d33a52a14a8ba1441e171cde96fd8fb4455a
genjix <genjix@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-03-31 15:13:33 +0000

    - Genjix fixed boulderdash by removing the pcfactory clauses.

--- scripts/smallgame/smallgame.py
+++ scripts/smallgame/smallgame.py
@@ -4,18 +4,6 @@ from blcelc import *
 class smallgame:
 	def __init__(self,celEntity):
 		print "Initializing game..."
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.region")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.mesh")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.solid")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.defaultcamera")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.gravity")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.movable")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.pccommandinput")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.linmove")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.actormove")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.colldet")
-		celRegisterPCFactory(object_reg_ptr,"cel.pcfactory.timer")
-
 		region = celCreateRegion(physicallayer_ptr,celEntity,"main")
 		# @@@ Ugly hardcoding of path!
 		region.SetWorldFile("/cellib/objects", "portal_world")
