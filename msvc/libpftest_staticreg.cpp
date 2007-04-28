9227d449a6202c2d0f68170aabd001278318a9eb
crystal-manage <crystal-manage@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-04-28 01:16:00 +0000

    Visual-C++ project files: Automated Visual-C++ project file repair. Swig Python files: Automated Swig Python file repair. User's Manual: Automated Texinfo to HTML conversion.

--- msvc/libpftest_staticreg.cpp
+++ msvc/libpftest_staticreg.cpp
@@ -21,6 +21,12 @@ static char const metainfo_pftest[] =
 "<plugin>"
 "  <scf>"
 "    <classes>"
+"      <!-- @@@ to be deprecated -->"
+"      <class>"
+"        <name>cel.pcfactory.test</name>"
+"        <implementation>celPfTest</implementation>"
+"        <description>CEL Test Property Class Factory</description>"
+"      </class>"
 "      <class>"
 "        <name>cel.pcfactory.misc.test</name>"
 "        <implementation>celPfTest</implementation>"
@@ -34,10 +40,18 @@ static char const metainfo_pftest[] =
   #define celPfTest_FACTORY_REGISTER_DEFINED 
     SCF_DEFINE_FACTORY_FUNC_REGISTRATION(celPfTest) 
   #endif
+  #ifndef celPfTest_FACTORY_REGISTER_DEFINED 
+  #define celPfTest_FACTORY_REGISTER_DEFINED 
+    SCF_DEFINE_FACTORY_FUNC_REGISTRATION(celPfTest) 
+  #endif
 
 class pftest
 {
 SCF_REGISTER_STATIC_LIBRARY(pftest,metainfo_pftest)
+  #ifndef celPfTest_FACTORY_REGISTERED 
+  #define celPfTest_FACTORY_REGISTERED 
+    celPfTest_StaticInit celPfTest_static_init__; 
+  #endif
   #ifndef celPfTest_FACTORY_REGISTERED 
   #define celPfTest_FACTORY_REGISTERED 
     celPfTest_StaticInit celPfTest_static_init__; 
