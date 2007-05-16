0b23cf7d6c797d77d8e6a5d3760c2a7803ff3991
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-05-16 12:05:58 +0000

    - Jorrit fixed the celconsole that it always renders on top of 	  everything (including billboards) and it will also eat events 	  so that billboards and game no longer get them. This fixes 	  trac #16.

--- cel/trunk/plugins/tools/celconsole/celconsole.h
+++ cel/trunk/plugins/tools/celconsole/celconsole.h
@@ -24,6 +24,7 @@
 #include "csutil/hash.h"
 #include "csutil/weakrefarr.h"
 #include "csutil/stringarray.h"
+#include "csutil/eventhandlers.h"
 #include "iutil/comp.h"
 #include "iutil/eventh.h"
 #include "iutil/eventq.h"
@@ -140,7 +141,39 @@ public:
       return parent->HandleEvent (ev);
     }
     CS_EVENTHANDLER_NAMES("cel.tools.celconsole")
-    CS_EVENTHANDLER_NIL_CONSTRAINTS
+    CS_CONST_METHOD virtual const csHandlerID * GenericPrec(
+      csRef<iEventHandlerRegistry> &r1, csRef<iEventNameRegistry> &r2,
+      csEventID e) const
+    {
+      if (e == csevFrame (r2))
+      {
+        /* TODO : not thread-safe */
+        static csHandlerID precs[2];
+	precs[0] = FrameSignpost_2DConsole::StaticID (r1);
+	precs[1] = CS_HANDLERLIST_END;
+        return precs;
+      }
+      else return 0;
+    }
+    CS_CONST_METHOD virtual const csHandlerID * GenericSucc(
+      csRef<iEventHandlerRegistry> &r1, csRef<iEventNameRegistry> &r2,
+      csEventID e) const
+    {
+      if (e == csevKeyboardEvent (r2) || e == csevMouseEvent (r2))
+      {
+        /* TODO : not thread-safe */
+        static csHandlerID precs[4];
+	precs[0] = FrameSignpost_Logic3D::StaticID (r1);
+	precs[1] = r1->GetGenericID ("cel.propclass");
+	precs[2] = r1->GetGenericID ("cel.tools");
+	precs[3] = CS_HANDLERLIST_END;
+        return precs;
+      }
+      else return 0;
+    }
+    
+    CS_EVENTHANDLER_DEFAULT_INSTANCE_CONSTRAINTS
+
   } *scfiEventHandler;
 };
 
