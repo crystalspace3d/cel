fd6854212d2df8601f2a2b0fa77df5dc46ff8263
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2007-08-09 07:14:11 +0000

    Compile fix for removal of debug.h

--- plugins/propclass/damage/damagefact.cpp
+++ plugins/propclass/damage/damagefact.cpp
@@ -21,7 +21,6 @@
 #include "csgeom/math3d.h"
 #include "iutil/objreg.h"
 #include "iutil/object.h"
-#include "csutil/debug.h"
 #include "iengine/mesh.h"
 #include "iengine/movable.h"
 #include "iengine/sector.h"
