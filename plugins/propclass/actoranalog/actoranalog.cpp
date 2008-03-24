a803e7bfa31c3c682d0cbe299b3c31105518c609
genjix <genjix@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2008-03-24 17:32:03 +0000

    - Genjix added target_axis speed deflection.

--- plugins/propclass/actoranalog/actoranalog.cpp
+++ plugins/propclass/actoranalog/actoranalog.cpp
@@ -256,7 +256,7 @@ void celPcActorAnalog::UpdateMovement ()
   if (ABS(delta_rot) < 0.1)
   {
     // move forwards
-    pclinmove->SetVelocity (csVector3 (0, 0, -movespeed));
+    pclinmove->SetVelocity (csVector3 (0, 0, -movespeed * target_axis.Norm()));
     pcmesh->SetAnimation ("walk", true);
   }
   else
