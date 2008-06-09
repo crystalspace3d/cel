23328e9696c84a34bf3491b708ae0ba75e44ce9a
caedesv <caedesv@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2008-06-09 16:36:07 +0000

    changed linmove path following to clamp to end time of path, otherwise it can get a position from out of the path, and dissapear.

--- cel/trunk/plugins/propclass/move/linmove.cpp
+++ cel/trunk/plugins/propclass/move/linmove.cpp
@@ -1043,6 +1043,13 @@ void celPcLinearMovement::ExtrapolatePosition (float delta)
   if (path)
   {
     path_time += delta;
+    bool finished = false;
+    float end_time = path->GetTime (path->Length () - 1);
+    if (path_time>end_time)
+    {
+      finished = true;
+      path_time = end_time;
+    }
     path->CalculateAtTime (path_time);
     csVector3 pos, look, up;
 
@@ -1063,7 +1070,7 @@ void celPcLinearMovement::ExtrapolatePosition (float delta)
     {
       spstate->SetAction (path_actions[path->GetCurrentIndex ()]);
     }
-    if (path_time>path->GetTime (path->Length () - 1))
+    if (finished)
     {
       path = 0;
       path_time = 0;
