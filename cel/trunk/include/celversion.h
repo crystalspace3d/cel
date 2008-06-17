8b328564a55cb7b642bb018f2ab7a77ac1ecefad
vknecht <vknecht@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2008-06-17 13:17:50 +0000

    Bump version to 1.9.

--- cel/trunk/include/celversion.h
+++ cel/trunk/include/celversion.h
@@ -32,11 +32,11 @@
 /// Major version
 #define CEL_VERSION_MAJOR CS_VER_QUOTE(1)
 /// Minor version (even are stable versions, odd are development versions)
-#define CEL_VERSION_MINOR CS_VER_QUOTE(3)
+#define CEL_VERSION_MINOR CS_VER_QUOTE(9)
 /// Maintenance release / RC
 #define CEL_VERSION_BUILD CS_VER_QUOTE(0)
 /// Date of release
-#define CEL_RELEASE_DATE  CS_VER_QUOTE(Thu 24-May-2007)
+#define CEL_RELEASE_DATE  CS_VER_QUOTE(Thu 5-Jun-2008)
 
 /// A complete version number
 #define CEL_VERSION_NUMBER CS_VERSION_MAJOR "." CS_VERSION_MINOR "." \
