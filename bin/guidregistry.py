cd8540be40108b07b1a92a1a3d5cb6c66c50e730
res2002 <res2002@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2008-09-07 21:06:41 +0000

    Create file association for .celzips

--- bin/guidregistry.py
+++ bin/guidregistry.py
@@ -1,18 +1,4 @@
 guid_registry = {
     #We compute some uuids out of a base and a unique id (mostly component guids)
     "base": '6c754815-632d-4e1c-92f6-4ae951a76b18',
-
-    #Upgrade codes for our installers. Those may not change.
-    "cel.upgrade_code": '7C953DF2-BF93-11DC-B42B-0015582877AD',
-
-    #cel merge modules
-    "cel.doc": 'BFEEA872-BF93-11DC-B42B-0015582877AD',
-    "cel.vfs": 'C7E67122-BF93-11DC-B42B-0015582877AD',
-    "cel.register": 'D27841CE-BF93-11DC-B42B-0015582877AD',
-    "cel.data": 'DD058534-BF93-11DC-B42B-0015582877AD',
-    "cel.include": 'E639D420-BF93-11DC-B42B-0015582877AD',
-    "cel.arch.GCC_3.4.include": 'F65B66AC-BF93-11DC-B42B-0015582877AD',
-    "cel.arch.GCC_3.4.lib": '039A60DE-BF94-11DC-B42B-0015582877AD',
-    "cel.arch.GCC_3.4.executable": '06DF1D7A-BF94-11DC-B42B-0015582877AD',
-
     }
