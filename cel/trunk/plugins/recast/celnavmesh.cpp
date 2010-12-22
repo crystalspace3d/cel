99ba48e0057482332726b130a2adce97480e86b3
kickvb <kickvb@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2010-12-22 16:42:54 +0000

    Filename fixes, step 2

--- cel/trunk/plugins/recast/celnavmesh.cpp
+++ cel/trunk/plugins/recast/celnavmesh.cpp
@@ -16,9 +16,7 @@
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
-
-
-#include "CelNavMesh.h"
+#include "celnavmesh.h"
 
 CS_PLUGIN_NAMESPACE_BEGIN(celNavMesh)
 {
