eae08882f8d0d1ea3881f01050d80d89dd3dd020
kickvb <kickvb@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2011-03-20 02:41:06 +0000

    Added a first draft of a life simulator. It has currently a single 'Frankie' entity with very limited behaviour. The behaviour is made using the new behaviour tree of CEL.

---
+++ apps/lifesim/behave.h
@@ -0,0 +1,119 @@
+/*
+  Copyright (C) 2011 Christian Van Brussel, Eutyche Mukuama, Dodzi de Souza
+      Communications and Remote
+      Sensing Laboratory of the School of Engineering at the 
+      Universite catholique de Louvain, Belgium
+      http://www.tele.ucl.ac.be
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef BEHAVE_H
+#define BEHAVE_H
+
+#include <iutil/objreg.h>
+
+#include "physicallayer/pl.h"
+#include "behaviourlayer/bl.h"
+#include "behaviourlayer/behave.h"
+
+class BehaviourLayer : public scfImplementation1<BehaviourLayer, iCelBlLayer>
+{
+private:
+  iCelPlLayer* physicalLayer;
+
+public:
+  BehaviourLayer (iObjectRegistry* registry, iCelPlLayer* physicalLayer);
+  virtual ~BehaviourLayer ();
+
+  virtual const char* GetName () const { return "behaviourlayer"; }
+  virtual iCelBehaviour* CreateBehaviour (iCelEntity* entity, const char* name);
+
+  iObjectRegistry* registry;
+};
+
+class BehaviourCommon
+: public scfImplementation2<BehaviourCommon,
+  iCelBehaviour,
+  iCelTimerListener>
+{
+protected:
+  csString name;  
+  csRef<iCelEntity> entity;
+  csRef<BehaviourLayer> behaviourLayer;
+  csRef<iCelPlLayer> physicalLayer;
+
+public:
+  BehaviourCommon (const char* name, iCelEntity* entity,
+		   BehaviourLayer* behaviourLayer, iCelPlLayer* physicalLayer);
+  virtual ~BehaviourCommon ();
+
+  virtual void CreateBehaviourTree () = 0;
+
+  /**
+   * This is a specific version of SendMessage() that accepts
+   * an integer instead of a csStringID. Subclasses of ccBehaviourBase
+   * will only have to override this version. The normal iCelBehaviour
+   * versions of SendMessage() are implemented in this class and
+   * will redirect to this version.
+   */
+  virtual bool SendMessage (csStringID msg_id, iCelPropertyClass* pc, celData& ret, 
+                            iCelParameterBlock* params, va_list arg);
+
+  virtual iCelBlLayer* GetBehaviourLayer () const;
+  virtual bool SendMessage (const char* msg_id, iCelPropertyClass* pc, celData& ret, 
+                            iCelParameterBlock* params, ...);
+  virtual bool SendMessageV (const char* msg_id, iCelPropertyClass* pc, celData& ret, 
+                             iCelParameterBlock* params, va_list arg);
+  virtual void* GetInternalObject ();
+
+  virtual const char* GetName () const { return name.GetData (); }
+
+  //-- iCelTimerListener
+  virtual void TickEveryFrame () = 0;
+  virtual void TickOnce () = 0;
+};
+/*
+class LivingEntityBehaviour
+: public scfImplementation2<LivingEntityBehaviour,
+  //scfFakeInterface<iCelBehaviour>,
+  iCelBehaviour,
+  iCelTimerListener>,
+  public BehaviourCommon
+{
+ public:
+  LivingEntityBehaviour (iCelEntity* entity, BehaviourLayer* behaviourLayer,
+			 iCelPlLayer* physicalLayer);
+  virtual ~LivingEntityBehaviour () {}
+
+  //-- BehaviourCommon
+  virtual bool Initialize () { CreateBehaviourTree (); return true; }
+
+  virtual void CreateBehaviourTree () = 0;
+
+  virtual bool SendMessage (csStringID msg_id, iCelPropertyClass* pc, celData& ret, 
+                            iCelParameterBlock* params, va_list arg) = 0;
+
+  virtual const char* GetName () const { return name.GetData (); }
+
+  //-- iCelTimerListener
+  void TickEveryFrame () {}
+  void TickOnce () {}
+
+ protected:
+  // The name of this behaviour
+  csString name;
+};
+*/
+#endif
