94f1672bf3746491ac17d752d9408aecc8271a5c
smd501 <smd501@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2012-05-10 15:47:53 +0000

    Merged revisions 4861,4863-4871 via svnmerge from https://cel.svn.sourceforge.net/svnroot/cel/cel/trunk
    ........
      r4861 | jorrit | 2012-05-09 15:45:10 +0100 (Wed, 09 May 2012) | 3 lines
      
      Work on smooth scrolling in grid inventory.
    ........
      r4863 | jorrit | 2012-05-10 09:39:51 +0100 (Thu, 10 May 2012) | 3 lines
      
      Smooth scrolling now works nicely in the grid inventory.
    ........
      r4864 | jorrit | 2012-05-10 10:53:39 +0100 (Thu, 10 May 2012) | 4 lines
      
      More layout options for the grid inventory.
      Fixed a bug in getting the selected item.
    ........
      r4865 | jorrit | 2012-05-10 11:55:54 +0100 (Thu, 10 May 2012) | 3 lines
      
      Forgotten BeginDraw.
    ........
      r4866 | jorrit | 2012-05-10 13:28:14 +0100 (Thu, 10 May 2012) | 3 lines
      
      Fixed text drawing for the grid items.
    ........
      r4867 | kickvb | 2012-05-10 15:40:55 +0100 (Thu, 10 May 2012) | 2 lines
      
      Fixed plugin name
    ........
      r4868 | kickvb | 2012-05-10 15:49:36 +0100 (Thu, 10 May 2012) | 2 lines
      
      Added a missing linking entry to celtool
    ........
      r4869 | kickvb | 2012-05-10 15:56:05 +0100 (Thu, 10 May 2012) | 2 lines
      
      Added a missing entry to CEGUI for static linking
    ........
      r4870 | jorrit | 2012-05-10 16:11:47 +0100 (Thu, 10 May 2012) | 3 lines
      
      Put back the cel.ui.inventory.grid. Why was this replaced?
    ........
      r4871 | kickvb | 2012-05-10 16:21:39 +0100 (Thu, 10 May 2012) | 2 lines
      
      Regenerated MSVC files
    ........

--- cel/branches/soc2012/ai/plugins/tools/uitools/gridinv/gridinv.h
+++ cel/branches/soc2012/ai/plugins/tools/uitools/gridinv/gridinv.h
@@ -102,8 +102,9 @@ struct GridEntry
   csRef<iMeshWrapper> mesh;
   iCelEntity* entity;
   iCelEntityTemplate* tpl;
+  uint8 alpha;
 
-  GridEntry () : entity (0), tpl (0) { }
+  GridEntry () : entity (0), tpl (0), alpha (0) { }
 
   void SetupEntry (celUIGridInventory* inv,
       const char* text, int amount,
@@ -114,38 +115,48 @@ struct GridEntry
 class Layouter
 {
 public:
-  virtual void Setup (celUIGridInventory* inv, csArray<GridEntry>& grid) = 0;
-  virtual void Layout (celUIGridInventory* inv, csArray<GridEntry>& grid) = 0;
+  virtual void Layout () = 0;
   virtual void Scroll (int d, float time) = 0;
-  virtual void UpdateScroll (celUIGridInventory* inv, csArray<GridEntry>& grid,
-      float elapsed) = 0;
-  virtual GridEntry* GetSelected (celUIGridInventory* inv, csArray<GridEntry>& grid) = 0;
+  virtual void UpdateScroll (float elapsed) = 0;
+  virtual GridEntry* GetSelected () = 0;
+
+  /// Get the real position of a grid entry. Returns false if not visible.
+  virtual bool GetRealPosition (size_t i, int& x, int& y) = 0;
+  virtual bool SetStyleOption (const char* name, const char* value) = 0;
 };
 
 class GridLayouter : public Layouter
 {
 private:
+  celUIGridInventory* inv;
+
   bool verticalscroll;
   int horcount, vercount;
+  bool horcountSet, vercountSet;
+
+  // Total dimension of the inventory not including margins.
+  int totalwidth, totalheight;
+  // Real left/top margin.
+  int leftmargin, topmargin;
+
   int ix, iy;
 
   int firstx, firsty;
-  float scrollTime;
+  float scrollTime, maxScrollTime;
+  int scrollDirection;
 
-  size_t FirstSlot ();
-  bool NextSlot ();
+  void FirstSlot ();
+  void NextSlot ();
 
 public:
-  GridLayouter (bool verticalscroll) :
-    verticalscroll (verticalscroll), firstx (0), firsty (0),
-    scrollTime (0.0f) { }
+  GridLayouter (celUIGridInventory* inv, bool verticalscroll);
   virtual ~GridLayouter () { }
-  virtual void Setup (celUIGridInventory* inv, csArray<GridEntry>& grid);
-  virtual void Layout (celUIGridInventory* inv, csArray<GridEntry>& grid);
+  virtual void Layout ();
   virtual void Scroll (int d, float time);
-  virtual void UpdateScroll (celUIGridInventory* inv, csArray<GridEntry>& grid,
-      float elapsed);
-  virtual GridEntry* GetSelected (celUIGridInventory* inv, csArray<GridEntry>& grid);
+  virtual void UpdateScroll (float elapsed);
+  virtual GridEntry* GetSelected ();
+  virtual bool GetRealPosition (size_t i, int& x, int& y);
+  virtual bool SetStyleOption (const char* name, const char* value);
 };
 
 
@@ -253,6 +264,8 @@ public:
   iVirtualClock* GetVC () const { return vc; }
   iMouseDriver* GetMouseDriver () const { return mouse; }
   const InvStyle& GetStyle () const { return style; }
+  const csArray<GridEntry>& GetGrid () const { return grid; }
+  csArray<GridEntry>& GetGrid () { return grid; }
 
   void Refresh ();
 
@@ -267,7 +280,8 @@ public:
 
   virtual bool SetStyleOption (const char* name, const char* value)
   {
-    return style.SetStyleOption (this, name, value);
+    if (style.SetStyleOption (this, name, value)) return true;
+    return layouter->SetStyleOption (name, value);
   }
   virtual bool Bind (const char* eventname, const char* command, const char* args);
 
