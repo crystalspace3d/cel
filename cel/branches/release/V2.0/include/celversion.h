a0476719bfc5ef52aaf3509b1decbb8aa3a90d92
philwyett <philwyett@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2012-07-01 23:27:36 +0000

    Updated docs date and release date.
    Kind of a give away of when the release will be. ;-)

--- cel/branches/release/V2.0/include/celversion.h
+++ cel/branches/release/V2.0/include/celversion.h
@@ -1,16 +1,16 @@
 /*
     Copyright (C) 1998-2007 by Jorrit Tyberghein
-  
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-  
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-  
+
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -36,7 +36,7 @@
 /// Maintenance release / RC
 #define CEL_VERSION_BUILD CS_VER_QUOTE(0)
 /// Date of release
-#define CEL_RELEASE_DATE  CS_VER_QUOTE(Tue 7-Feb-2012)
+#define CEL_RELEASE_DATE  CS_VER_QUOTE(Tue 03-July-2012)
 
 /// A complete version number
 #define CEL_VERSION_NUMBER CS_VERSION_MAJOR "." CS_VERSION_MINOR "." \
