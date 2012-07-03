061857f41ee6f33aa3b0465cd925e38a3398835d
smd501 <smd501@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2012-07-03 09:32:00 +0000

    -Adding relevant files for making a BT loader addon (based on dynworldload)

---
+++ ai/plugins/addons/behaviourtreeload/behaviourtreeload.h
@@ -0,0 +1,71 @@
+/*
+    Crystal Space Entity Layer
+    Copyright (C) 2011 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __CEL_ADDON_BTLOAD__
+#define __CEL_ADDON_BTLOAD__
+
+#include "csutil/scf.h"
+#include "csutil/scf_implementation.h"
+#include "iutil/comp.h"
+#include "iengine/engine.h"
+#include "imap/reader.h"
+#include "imap/writer.h"
+#include "csutil/strhash.h"
+
+#include "physicallayer/pl.h"
+#include "propclass/dynworld.h"
+#include "tools/dynworldload.h"
+
+struct iDynamicFactory;
+
+class celAddOnBehaviourTreeLoader : public scfImplementation3<celAddOnBehaviourTreeLoader,
+  iLoaderPlugin, iSaverPlugin, iComponent>
+{
+private:
+  iObjectRegistry* object_reg;
+  csRef<iCelPlLayer> pl;
+  csRef<iPcDynamicWorld> dynworld;
+  csRef<iSyntaxService> synldr;
+  csRef<iEngine> engine;
+  csStringHash xmltokens;
+  csRefArray<iDynamicWorldLoaderExtension> extensions;
+
+  bool ParseFactory (iDocumentNode* node);
+  bool WriteBodies (iDocumentNode* factNode, iDynamicFactory* fact);
+  bool WritePivots (iDocumentNode* factNode, iDynamicFactory* fact);
+  bool WriteJoints (iDocumentNode* factNode, iDynamicFactory* fact);
+
+public:
+  celAddOnBehaviourTreeLoader (iBase *iParent);
+  virtual ~celAddOnBehaviourTreeLoader ();
+  virtual bool Initialize (iObjectRegistry *object_reg);
+
+  virtual csPtr<iBase> Parse (iDocumentNode* node,
+  	iStreamSource* ssource, iLoaderContext* ldr_context,
+  	iBase* context);
+
+  virtual bool IsThreadSafe() { return false; }
+
+  virtual bool WriteDown (iBase* obj, iDocumentNode* parent,
+  	iStreamSource* ssource);
+};
+
+
+#endif // __CEL_ADDON_BTLOAD__
+
