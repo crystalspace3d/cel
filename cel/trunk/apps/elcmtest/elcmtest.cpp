e08d8b8972b77d6a3a04ce6eabb56a2834846fd5
jorrit <jorrit@eea6dfeb-6503-4900-9b5d-5805b534e6a8>
2013-05-07 13:34:17 +0000

    Fixed font problem after latest CS changes.

--- cel/trunk/apps/elcmtest/elcmtest.cpp
+++ cel/trunk/apps/elcmtest/elcmtest.cpp
@@ -160,7 +160,7 @@ bool ElcmTest::InitWindowSystem ()
 
   cegui->GetSchemeManagerPtr ()->create("ice.scheme");
   cegui->GetSystemPtr ()->setDefaultMouseCursor("ice", "MouseArrow");
-  cegui->GetFontManagerPtr ()->createFreeTypeFont("DejaVuSans", 10, true, "/fonts/ttf/DejaVuSans.ttf");
+  cegui->GetFontManagerPtr ()->createFreeTypeFont("DejaVuSans", 10, true, "/fonts/dejavu/DejaVuSans.ttf");
 
   CEGUI::WindowManager* winMgr = cegui->GetWindowManagerPtr ();
 
